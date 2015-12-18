48d6034f48fa110aac349ad9b7a1b5cb02bf0d9e
Nicholas Cardot <nick@sitesketch101.com>
2015-12-18 15:59:44 -0600

    Moved plugin library call to main file.

--- functions/header-meta-tags.php
+++ functions/header-meta-tags.php
@@ -176,7 +176,6 @@ function sw_remove_filter($hook_name = '', $method_name = '', $priority = 0 ) {
 			add_action( 'admin_head'   , 'sw_output_font_css' , 10);
 
 			// Disable Simple Podcast Press Open Graph tags
-			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 			if ( is_plugin_active( 'simple-podcast-press/simple-podcast-press.php' ) ) {
 				global $ob_wp_simplepodcastpress;
 				remove_action( 'wp_head' , array( $ob_wp_simplepodcastpress , 'spp_open_graph') , 1);
