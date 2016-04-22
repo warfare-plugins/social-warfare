df17024b5160cd2a461af61d7c3dff19c6e43f96
Nicholas Cardot <nick@sitesketch101.com>
2016-04-22 15:32:42 -0500

    Publishing v1.4.6

--- social-warfare.php
+++ social-warfare.php
@@ -3,7 +3,7 @@
 Plugin Name: Social Warfare
 Plugin URI: http://warfareplugins.com
 Description: A plugin to maximize social shares and drive more traffic using the fastest and most intelligent share buttons on the market, calls to action via in-post click-to-tweets, popular posts widgets based on share popularity, link-shortening, Google Analytics and much, much more!
-Version: 1.4.5
+Version: 1.4.6
 Author: Warfare Plugins
 Author URI: http://warfareplugins.com
 */
@@ -12,7 +12,7 @@ Author URI: http://warfareplugins.com
 *   VERSION AND DIRECTORIES							             *
 *                                                                *
 ******************************************************************/
-$pluginVersion = '1.4.5';
+$pluginVersion = '1.4.6';
 define( 'SW_VERSION' , $pluginVersion);
 $pluginUrl = rtrim(plugin_dir_url( __FILE__ ),'/');
 $pluginDir = dirname(__FILE__);
@@ -57,7 +57,7 @@ require_once $pluginDir  . '/functions/social-networks/stumbleupon.php';
 require_once $pluginDir  . '/functions/social-networks/yummly.php';
 require_once $pluginDir  . '/functions/social-networks/email.php';
 require_once $pluginDir  . '/functions/social-networks/whatsapp.php';
-// require_once $pluginDir  . '/functions/social-networks/pocket.php';
+require_once $pluginDir  . '/functions/social-networks/pocket.php';
 require_once $pluginDir  . '/functions/social-networks/buffer.php';
 
 
