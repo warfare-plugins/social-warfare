01f3dcbabe507344639c5007cf0bcfd2aa32d817
Nicholas Cardot <nick@sitesketch101.com>
2016-05-11 17:41:46 -0500

    Fix the RWMB_Core error. Closes #160

--- meta-box/meta-box.php
+++ meta-box/meta-box.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Plugin Name: Meta Box
- * Plugin URI: https://metabox.io
- * Description: Create custom meta boxes and custom fields for any post type in WordPress.
- * Version: 4.8.5
- * Author: Rilwis
- * Author URI: http://www.deluxeblogtips.com
- * License: GPL2+
- * Text Domain: meta-box
- * Domain Path: /lang/
- */
 
 if ( defined( 'ABSPATH' ) && ! class_exists( 'RWMB_Loader' ) )
 {
