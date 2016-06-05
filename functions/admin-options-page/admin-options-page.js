574aefdaa5c1eace35ebca8c3f184fb3f9e4f7d1
Nicholas Cardot <nick@sitesketch101.com>
2016-06-05 16:34:21 -0500

    Tab nav and half of first tab prototyped. #194

--- functions/admin-options-page/admin-options-page.js
+++ functions/admin-options-page/admin-options-page.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+// Admin Tabs
+jQuery(document).ready(function() {
+	jQuery(document).on('click','.sw-tab-selector',function(event) {
+		event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+		var tab = jQuery(this).attr('data-link');
+		jQuery('.sw-admin-tab').hide();
+		jQuery('#'+tab).show();
+		jQuery('.sw-header-menu li').removeClass('sw-active-tab');
+		jQuery(this).parents('li').addClass('sw-active-tab');
+	});
+});
--- functions/admin-options-page/admin-options-page.php
+++ functions/admin-options-page/admin-options-page.php
@@ -57,16 +57,107 @@ function sw_plugin_options() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	?>
+
 	<div class="sw-grid sw-col-940 sw-top-menu">
     	<img class="sw-header-logo" src="<?php echo SW_PLUGIN_DIR ?>/functions/admin-options-page/images/social-warfare-light.png" />
 		<ul class="sw-header-menu">
-        	<li class="active"><a href="#"><span>Display</span></a></li>
-        	<li><a href="#"><span>Styles</span></a></li>
-        	<li><a href="#"><span>Social Identity</span></a></li>
-        	<li><a href="#"><span>Advanced</span></a></li>
-        	<li><a href="#"><span>Registration</span></a></li>
+        	<li class="sw-active-tab"><a class="sw-tab-selector" href="#" data-link="sw-display-tab"><span>Display</span></a></li>
+        	<li><a class="sw-tab-selector" href="#" data-link="sw-styles-tab"><span>Styles</span></a></li>
+        	<li><a class="sw-tab-selector" href="#" data-link="sw-social-identity-tab"><span>Social Identity</span></a></li>
+        	<li><a class="sw-tab-selector" href="#" data-link="sw-advanced-tab"><span>Advanced</span></a></li>
+        	<li><a class="sw-tab-selector" href="#" data-link="sw-registration-tab"><span>Registration</span></a></li>
 		</ul>
 	</div>
+	<div class="sw-clearfix"></div>
+	<div class="sw-admin-wrapper">
+		<!-- Begin Tabs Container -->
+    	<div class="sw-tabs-container sw-grid sw-col-700">
+        	
+            <!-- Begin Display Tab -->
+            <div id="sw-display-tab" class="sw-admin-tab sw-grid sw-col-940">
+        		
+                <!-- Begin The Social Networks -->
+                <h2>Social Networks</h2>
+                <p class="sw-subtitle">Drag & Drop to activate and order your share buttons.</p>
+				<img src="<?php echo SW_PLUGIN_DIR ?>/functions/admin-options-page/images/social-networks.png">
+                <div class="sw-admin-divider sw-clearfix"></div>
+                <!-- End Social Networks -->
+
+                <!-- Begin Share Counts -->
+                <h2>Share Counts</h2>
+                <p class="sw-subtitle">Use the toggles below to determine how to display your social proof.</p>
+
+                <div class="sw-grid sw-col-300">
+                	<p class="sw-checkbox-label">Button Counts</p>
+                </div>
+
+                <div class="sw-grid sw-col-300">
+                	<div class="sw-checkbox-toggle"></div>
+                </div>
+                
+                <div class="sw-grid sw-col-300 sw-fit">
+                </div>
+                
+                <div class="sw-clearfix"></div>
+                
+                <div class="sw-grid sw-col-300">
+                	<p class="sw-checkbox-label">Total Counts</p>
+                </div>
+
+                <div class="sw-grid sw-col-300">
+                	<div class="sw-checkbox-toggle"></div>
+                </div>
+                
+                <div class="sw-grid sw-col-300 sw-fit">
+                </div>
+                
+                <div class="sw-clearfix"></div>
+                
+                <div class="sw-grid sw-col-300">
+                	<p class="sw-checkbox-label">Minimum Shares</p>
+                </div>
+
+                <div class="sw-grid sw-col-300">
+                	<input type="text" class="sw-admin-input" placeholder="0"/>
+                </div>
+                
+                <div class="sw-grid sw-col-300 sw-fit">
+                </div>
+                
+                <div class="sw-admin-divider sw-clearfix"></div>
+                <!-- End Share Counts -->
+                
+			<!-- End Display Tab -->
+			</div>
+            
+            <!-- Begin Display Tab -->
+			<div id="sw-styles-tab" class="sw-admin-tab sw-grid sw-col-940">
+			
+            	Styles Tab
+            
+            <!-- End Display Tab -->
+            </div>
+            
+        
+        <!-- End Tabs Container -->
+        </div>
+        
+        
+		<!-- Begin Admin Sidebar -->        
+        <div class="sw-admin-sidebar sw-grid sw-col-220 sw-fit">
+            <img src="<?php echo SW_PLUGIN_DIR ?>/functions/admin-options-page/images/sidebar-button-save-changes.jpg">
+            <img src="<?php echo SW_PLUGIN_DIR ?>/functions/admin-options-page/images/sidebar-images.jpg">
+            <p class="sw-support-notice">Need help? Check out our Knowledgebase.</p>
+            <p class="sw-support-notice">Opening a support ticket? Copy your System Status by clicking the button below.</p>
+            <img src="<?php echo SW_PLUGIN_DIR ?>/functions/admin-options-page/images/sidebar-button-support.jpg">
+        
+        <!-- End Admin Sidebar -->
+        </div>
+	
+    
+    </div>
+    
+    
 	<?php
 }
 
