3f70af8bb110a7f622d4105bd7cfe4ed3b37f146
Nicholas Cardot <nick@sitesketch101.com>
2016-10-22 14:41:48 -0500

    Moved the mobile detection to client side to avoid caching collisions. Closes #74. Closes #143

--- functions/social-networks/whatsapp.php
+++ functions/social-networks/whatsapp.php
@@ -1,13 +1,9 @@
 <?php
 
 /**
-
- * **************************************************************
- *                                                                *
- *   #1: Add the On / Off Switch	and Sortable Option				 *
- *                                                                *
- ******************************************************************/
-	add_filter( 'swp_button_options', 'swp_whatsapp_options_function',20 );
+ * #1: Add the On / Off Switch	and Sortable Option
+ */
+add_filter( 'swp_button_options', 'swp_whatsapp_options_function',20 );
 function swp_whatsapp_options_function( $options ) {
 
 	// Create the new option in a variable to be inserted
@@ -21,17 +17,14 @@ function swp_whatsapp_options_function( $options ) {
 	return $options;
 
 };
+
 /**
+ * #2: Add it to global network array
+ */
+// Queue up your filter to be ran on the swp_options hook.
+add_filter( 'swp_add_networks', 'swp_whatsapp_network' );
 
-***************************************************************
-*                                                                *
-*   #2: Add it to global network array	         				 *
-*                                                                *
-*/
-	// Queue up your filter to be ran on the swp_options hook.
-	add_filter( 'swp_add_networks', 'swp_whatsapp_network' );
-
-	// Create the function that will filter the options
+// Create the function that will filter the options
 function swp_whatsapp_network( $networks ) {
 
 	// Add your network to the existing network array
@@ -40,37 +33,27 @@ function swp_whatsapp_network( $networks ) {
 	// Be sure to return the modified options array or the world will explode
 	return $networks;
 };
-/**
 
- * **************************************************************
- *                                                                *
- *   #3: Generate the API Share Count Request URL	             *
- *                                                                *
- ******************************************************************/
+/**
+ *  #3: Generate the API Share Count Request URL
+ */
 function swp_whatsapp_request_link( $url ) {
 	$request_url = 'https://api.facebook.com/restserver.php?method=links.getStats&format=json&urls=' . $url;
 	return 0;
 }
-/**
 
- * **************************************************************
- *                                                                *
- *   #4: Parse the Response to get the share count	             *
- *                                                                *
- ******************************************************************/
+/**
+ * #4: Parse the Response to get the share count
+ */
 function swp_format_whatsapp_response( $response ) {
 	// $response = json_decode($response, true);
 	// return isset($response[0]['total_count'])?intval($response[0]['total_count']):0;
 	return 0;
 }
 /**
-
-***************************************************************
-*                                                                *
-*   #5: Create the Button HTML				  		             *
-*                                                                *
-*/
-	add_filter( 'swp_network_buttons', 'swp_whatsapp_button_html',10 );
+ * #5: Create the Button HTML
+ */
+add_filter( 'swp_network_buttons', 'swp_whatsapp_button_html',10 );
 function swp_whatsapp_button_html( $array ) {
 
 	// If we've already generated this button, just use our existing html
@@ -78,7 +61,7 @@ function swp_whatsapp_button_html( $array ) {
 		$array['resource']['whatsapp'] = $_GLOBALS['sw']['buttons'][ $array['postID'] ]['whatsapp'];
 
 		// If not, let's check if WhatsApp is activated and create the button HTML
-		elseif ( (isset( $array['options']['newOrderOfIcons']['whatsapp'] ) && swp_mobile_detection() && ! isset( $array['buttons'] ))
+		elseif ( (isset( $array['options']['newOrderOfIcons']['whatsapp'] ) && ! isset( $array['buttons'] ))
 		|| (isset( $array['buttons'] ) && isset( $array['buttons']['whatsapp'] ))  ) :
 
 			$array['totes'] += $array['shares']['whatsapp'];
