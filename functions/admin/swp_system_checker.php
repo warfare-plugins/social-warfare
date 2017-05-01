856af0dce4d1b254372807d69dc0af74067686f5
Nicholas Cardot <nick@sitesketch101.com>
2017-05-01 14:50:26 -0500

    Consolidate the System Checks into a single file

--- functions/admin/swp_system_checker.php
+++ functions/admin/swp_system_checker.php
@@ -49,4 +49,67 @@ class swp_system_checker
     }
 }
 
+class swp_php_check extends swp_custom_check
+{
+	public function __construct()
+	{
+		$this->name = 'PHP Check';
+	}
+
+	public function run()
+	{
+		if( version_compare( PHP_VERSION, '5.2.0' ) >= 0 )
+		{
+			$this->check_passed = true;
+		}
+		else
+		{
+			$this->check_passed = false;
+			$this->whats_wrong = 'Your server is currently using PHP version 5.2 (or whatever version it is). In order for our plugin to fetch share counts properly, you must be using PHP 5.3 or newer.';
+			$this->how_to_fix = 'To fix this, simply contact your hosting provider and ask them to update your server to the latest stable version of PHP.  If possible, test your website on a newer version before updating to avoid errors that could cause problems on a live site.';
+		}
+
+		return $this->check_passed;
+	}
+}
+
+class swp_curl_check extends swp_custom_check
+{
+	public function __construct()
+	{
+		$this->name = 'Curl Check';
+	}
+
+	public function run()
+	{
+
+		if( function_exists( 'curl_version' ) )
+		{
+			$this->check_passed = true;
+		}
+		else
+		{
+			$this->check_passed = false;
+			$this->whats_wrong = 'Your server has cURL disabled. In order for our plugin to fetch share counts, you must have cURL enabled on your server.';
+			$this->how_to_fix = 'To fix this, simply contact your hosting provider and ask them to activate cURL on your server.';
+		}
+
+		return $this->check_passed;
+	}
+}
+
+abstract class swp_custom_check
+{
+	public $name = "";
+	public $whats_wrong = "";
+	public $how_to_fix= "";
+	public $check_passed = null;
+	public $additional_message = null;
+
+	/**
+	 * Force children to have an executable run method.
+	 */
+	abstract public function run();
+}
+
 ?>
