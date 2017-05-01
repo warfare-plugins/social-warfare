a2b3ce03314192ff5fe7c6ff2aa80ab17cebf83f
Nicholas Cardot <nick@sitesketch101.com>
2017-05-01 15:08:16 -0500

    Eliminated inline control structures from system checker
    WordPress does not allow inline control structures. All conditionals
    must either use braces or use the alternative if/endif structures. You
    may not use shorthand conditionals that do not use any braces or endifs.

--- functions/admin/swp_system_checker.php
+++ functions/admin/swp_system_checker.php
@@ -58,23 +58,27 @@ class swp_system_checker
     {
 		foreach( swp_system_checker::$custom_checks as $custom_check )
 		{
-			if( method_exists( $custom_check, 'run' ) )
-				if( !$custom_check->run() && !$custom_check->check_passed )
+			if( method_exists( $custom_check, 'run' ) ) {
+				if( !$custom_check->run() && !$custom_check->check_passed ) {
 					swp_system_checker::$has_error = true;
+                }
+            }
 		}
     }
 
     public static function print_all_errors()
     {
-    	if( !isset( swp_system_checker::$has_error ) || empty( swp_system_checker::$has_error ) )
+    	if( !isset( swp_system_checker::$has_error ) || empty( swp_system_checker::$has_error ) ) {
     		return FALSE;
+        }
 
     	echo '<div class="sw-red-notice">';
 
     	foreach( swp_system_checker::$custom_checks as $custom_check )
     	{
-    		if( $custom_check->check_passed )
+    		if( $custom_check->check_passed ) {
     			continue;
+            }
 
     		echo '<br><p style="font-weight:bold">A problem has been found:</p>' . $custom_check->whats_wrong;
     		echo '<br><p style="font-weight:bold">Here is how to resolve the problem:</p>' . $custom_check->how_to_fix;
