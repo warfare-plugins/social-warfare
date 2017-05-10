7c8957396042bf32248346dc574b21e6e512f265
Nicholas Cardot <nick@sitesketch101.com>
2017-05-10 13:29:45 -0500

    Added filter to remove all shortcodes from excerpts

--- functions/utilities/utility.php
+++ functions/utilities/utility.php
@@ -110,40 +110,40 @@ function swp_get_excerpt_by_id( $post_id ) {
 		$the_post = get_post( $post_id ); // Gets post ID
 		$the_excerpt = $the_post->post_excerpt;
 
-		// If not, let's create an excerpt
-		else :
-			$the_post = get_post( $post_id ); // Gets post ID
-			$the_excerpt = $the_post->post_content; // Gets post_content to be used as a basis for the excerpt
-		endif;
+	// If not, let's create an excerpt
+	else :
+		$the_post = get_post( $post_id ); // Gets post ID
+		$the_excerpt = $the_post->post_content; // Gets post_content to be used as a basis for the excerpt
+	endif;
 
-		$excerpt_length = 100; // Sets excerpt length by word count
+	$excerpt_length = 100; // Sets excerpt length by word count
 
-		// Filter out any inline script or style tags as well as their content
-		if( !empty( $the_excerpt ) ):
-			$html = new DOMDocument();
-			$html->loadHTML($the_excerpt);
-			swp_remove_elements_by_tag_name('script', $html);
-			swp_remove_elements_by_tag_name('style', $html);
-			$the_excerpt = $html->saveHtml();
-		endif;
+	// Filter out any inline script or style tags as well as their content
+	if( !empty( $the_excerpt ) ):
+		$html = new DOMDocument();
+		$html->loadHTML($the_excerpt);
+		swp_remove_elements_by_tag_name('script', $html);
+		swp_remove_elements_by_tag_name('style', $html);
+		$the_excerpt = $html->saveHtml();
+	endif;
 
-		$the_excerpt = strip_tags( strip_shortcodes( $the_excerpt ) ); // Strips tags and images
+	$the_excerpt = strip_tags( strip_shortcodes( $the_excerpt ) ); // Strips tags and images
+	$the_excerpt = preg_replace( '/\[[^\]]+\]/', '', $the_excerpt );
+	$the_excerpt = str_replace( ']]>', ']]&gt;', $the_excerpt );
+	$the_excerpt = strip_tags( $the_excerpt );
+	$excerpt_length = apply_filters( 'excerpt_length', 100 );
+	$excerpt_more = apply_filters( 'excerpt_more', ' ' . '[...]' );
+	$words = preg_split( "/[\n\r\t ]+/", $the_excerpt, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY );
 
-		$the_excerpt = str_replace( ']]>', ']]&gt;', $the_excerpt );
-		$the_excerpt = strip_tags( $the_excerpt );
-		$excerpt_length = apply_filters( 'excerpt_length', 100 );
-		$excerpt_more = apply_filters( 'excerpt_more', ' ' . '[...]' );
-		$words = preg_split( "/[\n\r\t ]+/", $the_excerpt, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY );
+	if ( count( $words ) > $excerpt_length ) :
+		array_pop( $words );
+		// array_push($words, '…');
+		$the_excerpt = implode( ' ', $words );
+	endif;
 
-		if ( count( $words ) > $excerpt_length ) :
-			array_pop( $words );
-			// array_push($words, '…');
-			$the_excerpt = implode( ' ', $words );
-		endif;
+	$the_excerpt = preg_replace( "/\r|\n/", '', $the_excerpt );
 
-		$the_excerpt = preg_replace( "/\r|\n/", '', $the_excerpt );
-
-		return $the_excerpt;
+	return $the_excerpt;
 }
 
 if ( ! function_exists( 'swp_mobile_detection' ) ) {
