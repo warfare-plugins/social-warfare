b2ba6b6973d98007e1268904f564606c6c355186
ckmahoney <cortland.mahoney@gmail.com>
2018-02-20 17:08:23 -0500

    Updated 2017 copyright to 2018.

--- functions/admin/options-fetch.php
+++ functions/admin/options-fetch.php
@@ -3,7 +3,7 @@
  * Functions for getting and setting the plugin's options.
  *
  * @package   SocialWarfare\Functions
- * @copyright Copyright (c) 2017, Warfare Plugins, LLC
+ * @copyright Copyright (c) 2018, Warfare Plugins, LLC
  * @license   GPL-3.0+
  * @since     1.0.0
  */
