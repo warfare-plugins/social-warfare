bab0ce894f8072f4c1b5d03bb348ba8a84cc76c3
ckmahoney <cortland.mahoney@gmail.com>
2018-02-27 18:53:58 -0500

    Added SWP_Options_Page_Tab #506

---
+++ functions/admin/SWP_Options_Page_Tab.php
@@ -0,0 +1,29 @@
+<?php
+class SWP_Options_Page_Tab {
+
+	// The name of the tab.
+	var $name;
+	var $sections;
+	var $priority;
+
+	public function __construct() {
+
+	}
+
+	public function set_name( $name ) {
+		$this->name = $name;
+	}
+
+	public function sort_by_priority() {
+
+		/**
+		 * Take the $this->sections and sort them according to their priority. So a section
+		 * with a priority of 1 will show up before a section wwith a priority of 2. Again,
+		 * this will allow addons to add sections of options right in the middle of a tab.
+		 * Set a section to 3 and it will show up in between sections that have priorities
+		 * of 2 and 4.
+		 */
+
+	}
+
+}
