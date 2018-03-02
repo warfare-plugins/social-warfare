d6bd6cd6ccafcf2984b5bac09db9172b2e8c1cb5
Nicholas Cardot <nick@sitesketch101.com>
2018-03-02 10:49:24 -0600

    Change the network_count key.

--- functions/options/SWP_Options_Page.php
+++ functions/options/SWP_Options_Page.php
@@ -38,10 +38,9 @@ class SWP_Options_Page {
 		$this->tabs->display->sections->share_counts->set_title( 'Share Counts' , 'http://warfareplugins.com/the-knowledge-base-article-for-this-section-of-options' );
 		$this->tabs->display->sections->share_counts->set_description( 'This is the description' );
 
-
 		$_options = $this->tabs->display->sections->share_counts->options;
-		$_options->totals_for_each_button = new SWP_Options_Page_Option_Checkbox();
-		$_options->totals_for_each_button->set_name( __( 'Button Counts' ,'social-warfare' ) )->set_priority(10)->set_size('two-thirds')->set_default(true)->set_premium('pro');
+		$_options->network_count = new SWP_Options_Page_Option_Checkbox();
+		$_options->network_count->set_name( __( 'Button Counts' ,'social-warfare' ) )->set_priority(10)->set_size('two-thirds')->set_default(true)->set_premium('pro');
 		/*
 		$this->tabs->display->sections->share_counts->options->totals_for_each_button = new SWP_Options_Page_Option(
 			array(
