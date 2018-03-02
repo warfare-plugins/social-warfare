ec6a99282c53b2555a5dc3818bde8bd580ab57bc
Nicholas Cardot <nick@sitesketch101.com>
2018-03-02 10:16:37 -0600

    Cleaning some syntax and reference erros.

--- functions/options/SWP_Options_Page_Option_Checkbox.php
+++ functions/options/SWP_Options_Page_Option_Checkbox.php
@@ -4,10 +4,11 @@ class SWP_Options_Page_Option_Checkbox extends SWP_Options_Page_Option {
 
 	public function __construct() {
 		parent::__construct();
+		$this->type = 'checkbox';
 	}
 
 	public function output_html() {
-		// Stuff goes here 
+		// Stuff goes here
 	}
 
 }
