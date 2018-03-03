00a24f27971a99a1d5f18e3508e3bb094d81b804
Nicholas Cardot <nick@sitesketch101.com>
2018-03-03 02:52:10 -0600

    Removed set_title. Replaced with set_name from trait for consistency. #503

--- functions/options/SWP_Options_Page_Section.php
+++ functions/options/SWP_Options_Page_Section.php
@@ -2,6 +2,13 @@
 
 class SWP_Options_Page_Section {
 
+
+	/**
+	 * SWP_Abstact Trait: Usuful methods able to be used by classes throughout the plugin.
+	 *
+	 */
+	use SWP_Options_Abstract;
+
 	public $name;
 	public $description;
 	public $information_link;
@@ -12,37 +19,34 @@ class SWP_Options_Page_Section {
 		$this->options = new stdClass();
 	}
 
-    // KB stands for knowledge base articles. We have one for every section of options.
-	public function set_title( $name , $information_link ) {
-		$this->name = $name;
-		$this->information_link = $information_link;
+
+	public function set_link( $link ) {
+
+		if ( !is_string( $link ) ) {
+			$this->throw(__CLASS__ . " method set_name() requires a string. {$link} is not a string." );
+		}
+
+		$this->link = $link;
+
+		return $this;
 	}
 
 	public function set_description( $description ) {
+
+		if ( !is_string( $description ) ) {
+			$this->throw(__CLASS__ . " method set_name() requires a string. {$description} is not a string." );
+		}
+
 		$this->description = $description;
+
+		return $this;
 	}
 
-    public function set_priority( $priority ) {
-        if ( ! intval( $priority ) ) {
-            return false;
-        }
 
-        $this->priority = $priority;
-
-        return $priority;
-    }
-
-	function sort_by_priority() {
-
-		/**
-		 * Take the $this->options array and sort it according to the priority attribute of each option.
-		 * This will allow us to add options via the addons and place them where we want them by simply
-		 * assigning a priority to it that will place it in between the two other options that are
-		 * immediately higher and lower in their priorities. Or place it at the end if it has the highest
-		 * priority.
-		 *
-		 */
-
+	public function render_html() {
+		// Render the opening div for the section.
+		// Loop through each option within this section and call their render_html() function.
+		// Render the closing div for the section including the horizontal rule/divider.
 	}
 
 }
