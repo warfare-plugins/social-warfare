8b3b334bdbcfe8562047fbb3b2ea86d6f194b1c0
Nicholas Cardot <nick@sitesketch101.com>
2018-03-02 21:29:34 -0600

    Built the sort_by_priority method. #503 #508

--- functions/options/SWP_Options_Page.php
+++ functions/options/SWP_Options_Page.php
@@ -51,6 +51,9 @@ class SWP_Options_Page {
 		$this->tabs->display->sections->share_counts->set_description( __( 'Use the toggles below to determine how to display your social proof.' , 'social-warfare' ) );
 		$_options = $this->tabs->display->sections->share_counts->options;
 
+	   $this->tabs->display->sections->test_section = new SWP_Options_Page_Section();
+	   $this->tabs->display->sections->test_section->set_priority( 30 );
+
 		// Display share counts on each button?
 		$_options->network_count = new SWP_Option_Checkbox();
 		$_options->network_count->set_name( __( 'Button Counts', 'social-warfare' ) )->set_priority(10)->set_size('two-thirds')->set_default(true);
@@ -67,14 +70,9 @@ class SWP_Options_Page {
 		 * basic "styles" features of the plugin.
 		 *
 		 */
-		$this->tabs->advanced = new SWP_Options_Page_Tab();
-		$this->tabs->advanced->set_priority( 40 );
-		$this->tabs->advanced->set_name( 'Advanced' );
-
-
-		$this->tabs->styles = new SWP_Options_Page_Tab();
-		$this->tabs->styles->set_priority( 20 );
-		$this->tabs->styles->set_name( 'Styles' );
+ 		$this->tabs->styles = new SWP_Options_Page_Tab();
+ 		$this->tabs->styles->set_priority( 20 );
+ 		$this->tabs->styles->set_name( 'Styles' );
 
 
 		/**
@@ -96,6 +94,9 @@ class SWP_Options_Page {
 		 * basic "advanced" features of the plugin.
 		 *
 		 */
+ 		$this->tabs->advanced = new SWP_Options_Page_Tab();
+ 		$this->tabs->advanced->set_priority( 40 );
+ 		$this->tabs->advanced->set_name( 'Advanced' );
 
 		// TODO: This needs moved to a different tab and section.
 		// $_options->frame_buster = new SWP_Option_Checkbox();
@@ -105,14 +106,39 @@ class SWP_Options_Page {
 
 	}
 
+
+	/**
+	 * A method to sort tabs, section, and option according to their priority.
+	 *
+	 * This method will loop through all the tab objects, section objects, and
+	 * option objects and sort them according to their priority.
+	 *
+	 * @since 2.4.0 | 02 MAR 2018 | Created
+	 * @param null
+	 * @return null
+	 */
 	public function sort_by_priority() {
 
-		usort( $this->tabs , array( $this , 'compare' ) );
+		// Sort the tabs
+		$tabs = new ArrayObject( $this->tabs );
+        $tabs->uasort( array( $this , 'compare' ) );
 
-	}
+		// Loop through the tabs and sort the sections
+		foreach($this->tabs as $tab) {
 
-	public function compare($a, $b) {
-    	return strcmp($a->priority, $b->priority);
+			$sections = new ArrayObject( $tab->sections );
+        	$sections->uasort( array( $this , 'compare' ) );
+
+			// Loop through the sections and sort the options
+			foreach($sections as $section){
+
+				$options = new ArrayObject( $section->options );
+				$options->uasort( array( $this , 'compare' ) );
+
+			}
+
+		}
+
 	}
 
 }
--- functions/utilities/SWP_Abstract.php
+++ functions/utilities/SWP_Abstract.php
@@ -1,11 +1,22 @@
 <?php
 
-
+/**
+ * SWP_Abstract
+ *
+ * A series of useful methods that can be pulled in and easily used in classes
+ * throughout the plugin.
+ *
+ * @since 2.4.0 | 02 MAR 2018 | Created
+ *
+ */
 trait SWP_Abstract {
+
+
     /**
      * Give classes an error handlingmethod.
      * @param  mixed $message The message to send as an error.
      * @return object Exception
+     *
      */
 	public function throw( $message ) {
 		if ( is_string( $message ) ) {
@@ -14,4 +25,34 @@ trait SWP_Abstract {
             throw new Exception( var_dump( $message) );
         }
 	}
+
+	/**
+	 * A method used within sorting functions to compare two integers.
+	 *
+	 * @since  2.4.0 | 02 MAR 2018 | Created
+	 * @param  int $a The first integer being compared.
+	 * @param  int $b The second integer being compared.
+	 * @return int 1, 0, or -1
+	 *
+	 */
+	public function compare($a, $b) {
+    	return $this->intcmp($a->priority, $b->priority);
+	}
+
+
+	/**
+	 * A method to replace strcmp which compares integers instead of strings.
+	 *
+	 * @since  2.4.0 | 2 MAR 2018 | Created 
+	 * @param  int $a The first integer being compared.
+	 * @param  int $b The second integer being compared.
+	 * @return int 1, 0, or -1
+	 *
+	 */
+	public function intcmp($a,$b) {
+	    if((int)$a == (int)$b)return 0;
+	    if((int)$a  > (int)$b)return 1;
+	    if((int)$a  < (int)$b)return -1;
+	}
+
 }
