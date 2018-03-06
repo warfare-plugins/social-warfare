b28ba00bb566a0674f10f0d8d29158f1769fef1f
ckmahoney <cortland.mahoney@gmail.com>
2018-03-05 21:56:54 -0500

    Added methods for Toggle, Select, and Text inputs.

--- functions/options/SWP_Option_Input.php
+++ functions/options/SWP_Option_Input.php
@@ -1,17 +1,15 @@
 <?php
 
-class SWP_Options_Input {
+class SWP_Options_Input extends SWP_Options_Abstract {
     public $type;
     public $size;
-    public $content;
     public $default;
+    public $premium;
+    public $addon;
 
-    public function __construct() {
-        $this->name = "string";
-        $this->choices = array();
-        $this->toggle =
+    public function __construct( $name ) {
+        parent::__construct( $name );
     }
-
 
     public function render_HTML() {
         //* Intentionally left blank.
@@ -28,45 +26,95 @@ class SWP_Options_Input {
         return $this;
     }
 
-    public function set_default( $default ) {
-        $this->default = $defaults;
+    public function set_default() {
+        //* Intentionally left blank.
+        //* Each child class should override this method.
     }
 
-}
-
-
-class SWP_Options_Select extends SWP_Options_Input {
-    public function __construct( ) {
-
-    }
-
-    public function set_content( $content )  {
-
-        if ( !is_array( $content ) ) {
-            return false;
+    public function set_premium( $bool ) {
+        if (!is_boolean($bool) ) {
+            $this->throw("Requires a boolean, typically True. ");
         }
 
-        $this->content = $content;
+        $this->premium = $bool;
 
         return $this;
     }
 
+    /**
+     * Force a child option to be dependend on a parent option.
+     *
+     * @param string $parent The parent option's key.
+     * @param array $values Values which enable this option to exist.
+     *
+     */
+    public function set_dependency( $parent, $values ) {
+        if ( !is_string( $parent ) ) {
+            $this->throw( "Parent needs to be a string matching the key of another option." );
+        }
 
+        if ( !isset( $values) ) {
+            $this->throw( "Dependency values must be set." );
+        }
+
+        if ( !is_array( $values ) ) {
+            $values = array( $values );
+        }
+
+        $this->dependency = new stdObject();
+        $this->dependency->parent = $parent;
+        $this->dependency->values = $values;
+
+        return $this;
+    }
 }
 
-$select = new SWP_Select();
+/**
+ * For <input type="text" />
+ *
+ */
+class SWP_Options_Text extends SWP_Options_Input {
 
-$content = array(
-				'top' => __( 'Top of the Page' ,'social-warfare' ),
-				'bottom' => __( 'Bottom of the Page' ,'social-warfare' ),
-				'left' => __( 'On the left side of the page' ,'social-warfare' ),
-				'right' => __('On the right side of the page', 'social-warfare'),
-                );
+    public function __construct( $name ) {
+        parent::__construct( $name );
+        $this->default = '';
+    }
+}
 
-$select->set_size('two-fourths')->set_content( $content )->default( 'bottom' );
+class SWP_Options_Select extends SWP_Options_Input {
+    public $choices;
 
+    public function __construct( $name ) {
+        parent::__construct( $name );
 
+        $this->choices = array();
+    }
 
+    /**
+     * Create the options for a select dropdown.
+     *
+     * @param array $choices Array of strings to be translated and made into options.
+     * @return SWP_Options_Select $this This objec with the updated choices.
+     *
+     */
+    public function set_choices( $choices )  {
 
+        if ( !is_array( $choices ) ) {
+            return false;
+        }
 
-            
+        foreach( $choices as $index => $choice ) {
+            $choices[$index] = __( $choice, 'social-warfare' );
+        }
+
+        $this->choices = $choices;
+
+        return $this;
+    }
+
+    public function set_default( $bool ) {
+        if ( !is_boolean($default ) ) {
+            $this->throw( "Requires a boolean value." );
+        }
+    }
+}
