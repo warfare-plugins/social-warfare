b2e8d7f1b4b86ca9915826fe25abd99f1da250df
ckmahoney <cortland.mahoney@gmail.com>
2018-03-14 23:02:17 -0400

    Updated __construct and set_default, created render_html()

--- functions/options/SWP_Option_Text.php
+++ functions/options/SWP_Option_Text.php
@@ -10,18 +10,51 @@
  */
 class SWP_Option_Text extends SWP_Option {
 
-    public function __construct( $name ) {
-        parent::__construct( $name );
+    public function __construct( $name, $key ) {
+        parent::__construct( $name, $key );
         $this->default = '';
     }
 
     public function set_default( $default ) {
-        if ( !is_string( $default )  ) {
+        if ( is_numeric( $default) ) :
+            settype( $default, 'string' );
+        endif;
+
+        if ( !is_string( $default )  ) :
             $this->_throw( 'Please provide a default value as a string.' );
-        }
+        endif;
 
         $this->default = $default;
 
         return $this;
+    }
+
+    public function render_HTML() {
+        if ( isset( $this->user_options[$this->key] ) ) :
+            $value = $this->user_options[$this->key];
+        else:
+            $value = $this->default;
+        endif;
+
+        $html = '<div class="sw-grid sw-col-940 sw-option-container ' . $this->key . '_wrapper" ';
+
+        $html .= $this->render_dependency();
+        $html .= $this->render_premium();
+
+        $html .= '>';
+
+            $html .= '<div class="sw-grid sw-col-300">';
+                $html .= '<p class="sw-input-label">' . $this->name . '</p>';
+            $html .= '</div>';
+
+            $html .= '<div class="sw-grid sw-col-300">';
+                $html .= '<input name="' . $this->key . '" data-swp-name="' . $this->key . '"  type="text" class="sw-admin-input" placeholder="' . $this->default . '" value="' . $value . '" />';
+            $html .= '</div>';
+            $html .= '<div class="sw-grid sw-col-300 sw-fit"></div>';
+            $html .= '<div class="sw-clearfix"></div>';
+
+        $html .= '</div>';
+
+        return $html;
     }
 }
