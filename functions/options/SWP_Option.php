97da072ea10a779cee983337fe2493d31dd525de
ckmahoney <cortland.mahoney@gmail.com>
2018-03-27 15:36:36 -0400

    Alphabetized, docblocked, and tidied method names.

--- functions/options/SWP_Option.php
+++ functions/options/SWP_Option.php
@@ -1,22 +1,20 @@
 <?php
 
 /**
- * SWP_Option: The parent class used to creat individual options on the options page.
+ * The parent class for all Option_X types.
  *
  * This class is used to create each individual option throughout the options page.
- * It provides the framework for each of the various types of options that are
- * available like input, select, checkbox, etc. Each type of option will be accessed
- * through a child class extended from this class.
- *
- * This parent class will contain the common methods used for setting the name, the
- * premium status, the defaults, etc.
+ * It provides the framework for each of type of option that is
+ * available: input, select, checkbox, and textarea. Each of these options is
+ * instantiated through their respective class.
  *
  * @since  2.4.0   | Created | 02 MAR 2017
  * @access public
  *
  */
-
 class SWP_Option extends SWP_Abstract {
+
+
     /**
     * The type of HTML input element.
     *
@@ -27,6 +25,7 @@ class SWP_Option extends SWP_Abstract {
     */
     public $type;
 
+
     /**
     * The CSS class representing the size (width) of the input.
     *
@@ -35,12 +34,14 @@ class SWP_Option extends SWP_Abstract {
     */
     public $size;
 
+
     /**
     * The key for this option in the database.
     *
     * @var string $key
     */
     public $key;
+
 
     /**
     * The default value for the given input.
@@ -49,12 +50,14 @@ class SWP_Option extends SWP_Abstract {
     */
     public $default;
 
+
     /**
     * The string of HTML which creates the element.
     *
     * @var string $html
     */
     public $html;
+
 
     /**
     * Boolean indicating whether the plugin is registered or not.
@@ -64,13 +67,11 @@ class SWP_Option extends SWP_Abstract {
     public $swp_registration;
 
 
-	/**
-    * The __construct magic method.
+    /**
+    * The required constructor for PHP classes.
     *
-    * This method is used to instantiate the class.
-    *
-    * @param none
-    * @return none
+    * @param string $name The display name for the toggle.
+    * @param string $key The database key for the user setting.
     *
     */
     public function __construct( $name, $key ) {
@@ -82,13 +83,63 @@ class SWP_Option extends SWP_Abstract {
     }
 
 
+    /**
+    * Fetches the css class to match a given size given as a string.
+    *
+    * @param string $size The size of the element using SWP sizing.
+    * @return object $this Allows for method chaining.
+    */
+    protected function get_css_size() {
+        $map = [
+            'two-fourths'   => ' sw-col-460 ',
+            'two-thirds'    => ' sw-col-940 ',
+            'four-fourths'  => ' sw-col-620 ',
+        ];
+
+        if ( empty( $this->size) ) :
+            return $map['two-thirds'];
+        endif;
+
+        return $map[$this->size];
+    }
+
+
+    /**
+    * Get the pre-defined value of the option.
+    *
+    * @since April 15 2018
+    */
+    protected function get_value() {
+        if ( isset($this->value) ) {
+            return $this->value;
+        }
+
+        if ( isset( $this->user_options[$this->key] ) ) {
+            return $this->user_options[$this->key];
+        }
+
+        return $this->default;
+    }
+
+
+    /**
+    * Creates HTML based on the option's properties and user settings.
+    *
+    * @return void
+    */
+    public function render_HTML() {
+        //* Intentionally left blank.
+        //* Each child class should override this method.
+        $this->_throw( "Should not be called from the parent class." );
+    }
+
+
 	/**
     * Set the default value of this option. This value will be used until the plugin user changes the value
     * to something else and saves the options.
     *
     * @since 2.4.0 | 02 MAR 2018 | Created
     * @param mixed The default value will vary based on the kind of option being generated.
-    * @return object $this Allos for method chaining.
     *
     */
     public function set_default( $value ) {
@@ -99,48 +150,6 @@ class SWP_Option extends SWP_Abstract {
         $this->default = $value;
 
          return $this;
-    }
-
-
-	/**
-    * Some option types have multiple sizes that will determine their visual layout on the option
-    * page. This setter allows you to declare which one you want to use.
-    *
-    * @since 2.4.0 | 02 MAR 2018 | Created
-    * @param string The size of the option on the page (e.g. 'two-thirds').
-    * @return object $this Allows for method chaining.
-    *
-    */
-    public function set_size( $size ) {
-        $options = [ 'two-fourths', 'two-thirds', 'four-fourths' ];
-
-        if ( !in_array( $size, $options) ) {
-            $sizes = PHP_EOL;
-
-            foreach( $options as $option ) {
-                $sizes .= $option . PHP_EOL;
-            }
-
-            $this->_throw( "Please enter a valid size. Acceptable sizes are:" . $sizes );
-        }
-
-        $this->size = $size;
-
-        return $this;
-    }
-
-
-    /**
-    * Creates HTML based on the option's properties and user settings.
-    *
-    * @param  bool $echo If True, immediataly echoes the markup to the page.
-    *                    Else returns the markup as a string.
-    * @return string $html
-    */
-    public function render_HTML() {
-        //* Intentionally left blank.
-        //* Each child class should override this method.
-        $this->_throw( "Should not be called from the parent class." );
     }
 
 
@@ -181,7 +190,7 @@ class SWP_Option extends SWP_Abstract {
     * Assign the database key for this element.
     *
     * @param string $key The key which correlates to the input.
-    * @return SWP_Option
+    * @return object $this The calling instance, for method chaining.
     */
     public function set_key( $key ) {
         if ( !is_string( $key ) ) {
@@ -194,40 +203,30 @@ class SWP_Option extends SWP_Abstract {
     }
 
 
-    /**
-    * Fetches the css class to match a given size given as a string.
+	/**
+    * Some option types have multiple sizes that will determine their visual layout on the option
+    * page. This setter allows you to declare which one you want to use.
     *
-    * @param string $size The size of the element using SWP sizing.
-    * @return object $this Allows for method chaining.
-    */
-    protected function get_css_size() {
-        $map = [
-            'two-fourths'   => ' sw-col-460 ',
-            'two-thirds'    => ' sw-col-940 ',
-            'four-fourths'  => ' sw-col-620 ',
-        ];
-
-        if ( empty( $this->size) ) :
-            return $map['two-thirds'];
-        endif;
-
-        return $map[$this->size];
-    }
-
-    /**
-    * Get the pre-defined value of the option.
+    * @since 2.4.0 | 02 MAR 2018 | Created
+    * @param string The size of the option on the page (e.g. 'two-thirds').
+    * @return object $this The calling instance, for method chaining.
     *
-    * @since April 15 2018
     */
-    protected function get_value() {
-        if ( isset($this->value) ) {
-            return $this->value;
+    public function set_size( $size ) {
+        $options = [ 'two-fourths', 'two-thirds', 'four-fourths' ];
+
+        if ( !in_array( $size, $options) ) {
+            $sizes = PHP_EOL;
+
+            foreach( $options as $option ) {
+                $sizes .= $option . PHP_EOL;
+            }
+
+            $this->_throw( "Please enter a valid size. Acceptable sizes are:" . $sizes );
         }
 
-        if ( isset( $this->user_options[$this->key] ) ) {
-            return $this->user_options[$this->key];
-        }
+        $this->size = $size;
 
-        return $this->default;
+        return $this;
     }
 }
