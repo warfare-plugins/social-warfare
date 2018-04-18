c3890b72dc8dc87316bbbf9f3e4f7077898da0ff
ckmahoney <cortland.mahoney@gmail.com>
2018-04-18 13:57:16 -0400

    Unindented code

--- functions/frontend-output/buttons-floating.php
+++ functions/frontend-output/buttons-floating.php
@@ -22,35 +22,30 @@ function socialWarfareSideFloat() {
 	wp_reset_query();
 
 
-			$i = 0;
-			// Sort the buttons according to the user's preferences
-			if ( isset( $buttonsArray ) && isset( $buttonsArray['buttons'] ) ) :
-				foreach ( $buttonsArray['buttons'] as $key => $value ) :
-					if ( isset( $buttonsArray['resource'][ $key ] ) && $i <= 5 ) :
-						$assets .= $buttonsArray['resource'][ $key ];
-					endif;
-					++$i;
-				endforeach;
-			elseif ( $options['order_of_icons'] == 'manual' ) :
-				foreach ( $options['order_of_icons'] as $key => $value ) :
-					if ( isset( $buttonsArray['resource'][ $key ] ) && $i <= 5 ) :
-						$assets .= $buttonsArray['resource'][ $key ];
-					endif;
-					++$i;
-				endforeach;
-			elseif ( $options['order_of_icons'] == 'dynamic' ) :
-				arsort( $buttonsArray['shares'] );
-				foreach ( $buttonsArray['shares'] as $thisIcon => $status ) :
-					if ( isset( $buttonsArray['resource'][ $thisIcon ] ) && $i <= 5 ) :
-						$assets .= $buttonsArray['resource'][ $thisIcon ];
-					endif;
-					++$i;
-				endforeach;
+	$i = 0;
+	// Sort the buttons according to the user's preferences
+	if ( isset( $buttonsArray ) && isset( $buttonsArray['buttons'] ) ) :
+		foreach ( $buttonsArray['buttons'] as $key => $value ) :
+			if ( isset( $buttonsArray['resource'][ $key ] ) && $i <= 5 ) :
+				$assets .= $buttonsArray['resource'][ $key ];
 			endif;
+			++$i;
+		endforeach;
+	elseif ( $options['order_of_icons'] == 'manual' ) :
+		foreach ( $options['order_of_icons'] as $key => $value ) :
+			if ( isset( $buttonsArray['resource'][ $key ] ) && $i <= 5 ) :
+				$assets .= $buttonsArray['resource'][ $key ];
+			endif;
+			++$i;
+		endforeach;
+	elseif ( $options['order_of_icons'] == 'dynamic' ) :
+		arsort( $buttonsArray['shares'] );
+		foreach ( $buttonsArray['shares'] as $thisIcon => $status ) :
+			if ( isset( $buttonsArray['resource'][ $thisIcon ] ) && $i <= 5 ) :
+				$assets .= $buttonsArray['resource'][ $thisIcon ];
+			endif;
+			++$i;
+		endforeach;
+	endif;
 
-	
-
-			// echo $assets;
-
-		endif;
 }
