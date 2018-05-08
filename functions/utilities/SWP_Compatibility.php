14af4af2bc81828bb2f2d14a36814182a0ec1213
ckmahoney <cortland.mahoney@gmail.com>
2018-05-08 12:42:51 -0400

    2.4.0 => 3.0.0

--- functions/utilities/SWP_Compatibility.php
+++ functions/utilities/SWP_Compatibility.php
@@ -7,7 +7,7 @@
  * @copyright Copyright (c) 2018, Warfare Plugins, LLC
  * @license   GPL-3.0+
  * @since     1.0.0
- * @since     2.4.0 | 22 FEB 2018 | Refactored into a class-based system.
+ * @since     3.0.0 | 22 FEB 2018 | Refactored into a class-based system.
  *
  */
 class SWP_Compatibility {
--- functions/widgets/SWP_Widget.php
+++ functions/widgets/SWP_Widget.php
@@ -6,7 +6,7 @@
 * @package   SocialWarfare\Functions\Widgets
 * @copyright Copyright (c) 2018, Warfare Plugins, LLC
 * @license   GPL-3.0+
-* @since     2.4.0 | 22 FEB 2018 | Class Created
+* @since     3.0.0 | 22 FEB 2018 | Class Created
 *
 */
 class SWP_Widget {
@@ -15,7 +15,7 @@ class SWP_Widget {
 	/**
 	 * The magic method used to instantiate this class.
 	 *
-	 * @since  2.4.0
+	 * @since  3.0.0
 	 * @param  none
 	 * @return none
 	 * @access public
@@ -30,7 +30,7 @@ class SWP_Widget {
 	 * The function that runs on the widgets_init hook and registers
 	 * our widget with WordPress.
 	 *
-	 * @since  2.4.0
+	 * @since  3.0.0
 	 * @param  none
 	 * @return none
 	 */
