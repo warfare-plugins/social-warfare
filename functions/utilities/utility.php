bd80d23cdf610954978a26f5748e1f5f6cd5df7e
Nicholas Cardot <nick@sitesketch101.com>
2018-05-11 10:56:57 -0500

    Fix the args.

--- functions/utilities/utility.php
+++ functions/utilities/utility.php
@@ -129,7 +129,7 @@ function swp_store_the_settings() {
  }
 
  function social_warfare( $args = array() ) {
-    $buttons_panel = SWP_Buttons_Panel;
+    $buttons_panel = new SWP_Buttons_Panel( $args );
 	echo $buttons_panel->the_buttons();
  }
 
