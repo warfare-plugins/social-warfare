178ab3774118690007705d3009e628d1e09ed3a8
ckmahoney <cortland.mahoney@gmail.com>
2018-05-14 10:52:17 -0400

    Added require statement for legacy update-checker.php.

--- social-warfare.php
+++ social-warfare.php
@@ -22,6 +22,8 @@ define( 'SWP_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'SWP_PLUGIN_DIR', dirname( __FILE__ ) );
 define( 'SWP_STORE_URL', 'https://warfareplugins.com' );
 
+require_once SWP_PLUGIN_DIR . '/functions/legacy/update-checker.php';
+
 add_filter('the_excerpt', 'do_shortcode', 1);
 
 // Load the main Social_Warfare class and fire up the plugin.
