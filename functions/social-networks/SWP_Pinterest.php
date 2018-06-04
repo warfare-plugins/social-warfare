6e0f7c308fe09a48061af73e3d8c02115d366263
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-06-04 17:05:05 -0400

    Updated check for pinterest_image; whitespace.

--- functions/social-networks/SWP_Pinterest.php
+++ functions/social-networks/SWP_Pinterest.php
@@ -85,27 +85,30 @@ class SWP_Pinterest extends SWP_Social_Network {
      public function render_HTML( $panel_context , $echo = false ) {
         global $swp_user_options;
         $post_id = $panel_context['post_data']['ID'];
-				$post_url = urlencode( urldecode( SWP_URL_Management::process_url( $panel_context['post_data']['permalink'] , 'pinterest' , $post_id ) ) );
+		$post_url = urlencode( urldecode( SWP_URL_Management::process_url( $panel_context['post_data']['permalink'] , 'pinterest' , $post_id ) ) );
 
         $options = $swp_user_options;
         $pinterest_image = get_post_meta( $post_id , 'swp_pinterest_image' , true );
         $pinterest_image_link = get_post_meta( $post_id , 'swp_pinterest_image_url' , true );
 
+        if ( empty( $pinterest_image ) && isset($options['advanced_pinterest_fallback']) && $options['advanced_pinterest_fallback'] == 'featured' ):
+            $pinterest_image = wp_get_attachment_url( get_post_thumbnail_id( $post_id ) );
+        endif;
+
         if ( !empty( $options['pinterest_id'] ) ) :
-			 			$pinterest_username = ' via @' . str_replace( '@' , '' , $options['pinterest_id'] );
-		 		else :
-			 			$pinterest_username = '';
-		 		endif;
+ 			$pinterest_username = ' via @' . str_replace( '@' , '' , $options['pinterest_id'] );
+ 		else :
+ 			$pinterest_username = '';
+ 		endif;
 
         $title = str_replace( '|', '', strip_tags( $panel_context['post_data']['post_title'] ) );
-
         $pinterest_description	= get_post_meta( $post_id , 'swp_pinterest_description' , true );
 
-				if( is_array( $pinterest_description ) && !empty( $pinterest_description ) ) {
-						$pinterest_description = $pinterest_description[0];
-						// delete_post_meta( $post_id , 'swp_pinterest_description' );
-						update_post_meta( $post_id , 'swp_pinterest_description' , $pinterest_description );
-				}
+		if( is_array( $pinterest_description ) && !empty( $pinterest_description ) ) {
+			$pinterest_description = $pinterest_description[0];
+			// delete_post_meta( $post_id , 'swp_pinterest_description' );
+			update_post_meta( $post_id , 'swp_pinterest_description' , $pinterest_description );
+		}
 
         if ( empty( $pinterest_description ) ) :
             $pinterest_description = $title;
@@ -113,30 +116,21 @@ class SWP_Pinterest extends SWP_Social_Network {
 
         $pinterest_description .= $pinterest_username;
 
-        if ( $pinterest_image != '') :
-	       		$anchor = '<a rel="nofollow" class="nc_tweet" data-count="0" ' .
-								'data-link="https://pinterest.com/pin/create/button/' .
-								'?url=' . $panel_context['post_data']['permalink'] .
-								'&media=' . urlencode( $pinterest_image_link ) .
-								'&description=' . urlencode( $pinterest_description ) .
-								'">';
+        if ( !empty( $pinterest_image ) ) :
+       		$anchor = '<a rel="nofollow" class="nc_tweet" data-count="0" ' .
+						'data-link="https://pinterest.com/pin/create/button/' .
+						'?url=' . $panel_context['post_data']['permalink'] .
+						'&media=' . urlencode( $pinterest_image_link ) .
+						'&description=' . urlencode( $pinterest_description ) .
+					'">';
        	else :
-	       		$anchor = '<a rel="nofollow" class="nc_tweet noPop" ' .
-								'onClick="var e=document.createElement(\'script\');
-								   e.setAttribute(\'type\',\'text/javascript\');
-								   e.setAttribute(\'charset\',\'UTF-8\');
-								   e.setAttribute(\'src\',\'//assets.pinterest.com/js/pinmarklet.js?r=\'+Math.random()*99999999);
-								   document.body.appendChild(e);
-								" >';
-       	endif;
-
-        if ( isset($options['advanced_pinterest_fallback']) && $options['advanced_pinterest_fallback'] == 'featured'):
-         		$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $panel_context['postID'] ) );
-
-		     		if( !empty( $thumbnail_url ) ):
-			     			$array['imageURL'] = $thumbnail_url;
-		     		endif;
-
+       		$anchor = '<a rel="nofollow" class="nc_tweet noPop" ' .
+						'onClick="var e=document.createElement(\'script\');
+						   e.setAttribute(\'type\',\'text/javascript\');
+						   e.setAttribute(\'charset\',\'UTF-8\');
+						   e.setAttribute(\'src\',\'//assets.pinterest.com/js/pinmarklet.js?r=\'+Math.random()*99999999);
+						   document.body.appendChild(e);
+						" >';
        	endif;
 
          //* Begin parent class method.
@@ -145,6 +139,7 @@ class SWP_Pinterest extends SWP_Social_Network {
          $share_counts = $panel_context['shares'];
          $options = $panel_context['options'];
          $share_link = $this->generate_share_link( $post_data );
+
          // Build the button.
          $icon = '<span class="iconFiller">';
              $icon.= '<span class="spaceManWilly">';
@@ -152,11 +147,13 @@ class SWP_Pinterest extends SWP_Social_Network {
                  $icon.= '<span class="swp_share">' . $this->cta . '</span>';
              $icon .= '</span>';
          $icon .= '</span>';
+
          if ( true === $this->are_shares_shown( $share_counts , $options ) ) :
              $icon .= '<span class="swp_count">' . swp_kilomega( $share_counts[$this->key] ) . '</span>';
          else :
              $icon = '<span class="swp_count swp_hide">' . $icon . '</span>';
          endif;
+
          // Build the wrapper.
          $html = '<div class="nc_tweetContainer swp_'.$this->key.'" data-network="'.$this->key.'">';
              $html .= $anchor;
@@ -164,9 +161,10 @@ class SWP_Pinterest extends SWP_Social_Network {
                  $html .= $icon;
              $html.= '</a>';
          $html.= '</div>';
-         // Store these buttons so that we don't have to generate them for each set
 
+         // Store these buttons so that we don't have to generate them for each set
          $this->html = $html;
+         
          if ( $echo ) :
              echo $html;
          endif;
