805a7a5de549f520bdaaaca0c3b4c85d869fb775
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-06-05 10:41:31 -0400

    Finished the draft of StumbleUpon notice. Needs design review.

--- js/admin-options-page.js
+++ js/admin-options-page.js
@@ -110,21 +110,27 @@
     }
 
     function createNotice(message, parentElement) {
-        console.log("createNotice")
         var notice = document.createElement("div");
-        var style = "background-color: lemonchiffon; padding: 5px 10px; ";
+        var style = "background-color: #ced3dc; color: #30394f; padding: 5px 10px; font-size: 16px !important;";
+
+        var dismiss = document.createElement("span");
+        dismiss.textContent = "Dismiss this notice";
+
+        dismiss.addEventListener("click", function(e) {
+            jQuery(e.target.parentNode).slideUp();
+        });
 
         notice.style = style;
         notice.innerHTML = message;
+        notice.appendChild(dismiss)
 
         parentElement.appendChild(notice);
     }
 
     function createStumbleUponNotice() {
-        console.log("createStumbleUponNotice")
         var message = '<h3><u>Big news</u></h3>';
         message += '<p>As of June 30th, 2018, StumbleUpon will no longer exist as a sharing platform.<br/>Instead, they are moving in with Mix.</p>';
-        message += '<p>While this is existing for Mix, <b>share counts will not be transferred, and Mix is not providing a share button or API.</b>';
+        message += '<p>While this is exciting for Mix, <b>share counts will not be transferred, and Mix is not providing a share button or API.</b>';
         message += '<br/>You can read more about it <a href="http://help.stumbleupon.com/customer/en/portal/articles/2908172-transitioning-from-stumbleupon-to-mix" target="_blank">here</a>.</p>';
 
         var parent = document.querySelector(".social_networks_description_wrapper");
