e4728f9e394e02f8c16029dd57a27150840c47ba
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-06-05 14:25:36 -0400

    Updated methods and functionality.

--- functions/includes/SWP_Notice.php
+++ functions/includes/SWP_Notice.php
@@ -1,45 +1,25 @@
 <?php
 
 class SWP_Notice {
-    public function __construct( $key, $message, $type, $args ) {
-
+    public function __construct( $key, $message, $class = '' ) {
         $this->set_key( $key );
         $this->set_message( $message );
+        $this->set_class( $class );
         $this->notices = get_option( 'social_warfare_dismissed_notices', [] );
+        $this->actions = '';
 
         if ( !$this->should_display_notice() ) :
             return;
         endif;
-
-
-        if ( $type === 'dashboard' ) :
-                $this->notice = $this->create_dashboard_notice( $message );
-                add_action( 'admin_notices', [$this, 'print_HTML'] );
-
-        elseif ( $type = 'options_page' ) :
-                //* This class does not exist, but this code outlines the process
-                //* we would use to insert it into the options page somewhere.
-                $notice = new SWP_Option_Notice( $key, $message );
-                $notice->set_priority( $args['priority'] );
-                $tab = $args['tab'];
-                $section = $args['section'];
-                $SWP_Options_Page->tabs->$tab->sections->$section->add_option( $notice );
-
-        else :
-                // code...
-        endif;
-
-        add_action( 'wp_ajax_perma_dismiss', [ $this, 'perma_dismiss' ] );
-        add_action( 'wp_ajax_nopriv_perma_dismiss', [ $this, 'perma_dismiss' ] );
     }
 
-    public function should_display_notice() {
 
-        if ( true === $this->notices[$this->key] ) {
-            return false;
+    public function should_display_notice() {
+        if ( empty( $this->notices[$this->key] ) ) {
+            return true;
         }
 
-        return true;
+        return false === $this->notices[$this->key];
     }
 
 
@@ -52,40 +32,91 @@ class SWP_Notice {
 
 
     public function set_message( $message ) {
-        if ( 'string' !== gettype( $key ) ) :
+        if ( !is_string( $message ) ) :
             throw("Please provide a string for your database key.");
         endif;
+
+        $this->message = $message;
 
         return $this;
     }
 
 
-    public function set_key( $key ) {
-        if ( 'string' !== gettype( $key ) ) :
+    protected function set_key( $key ) {
+        if ( !is_string ( $key ) ) :
             throw("Please provide a string for your database key.");
         endif;
 
+        $this->key = $key;
+
         return $this;
     }
+
+    protected function set_class( $class ) {
+        if ( !is_string( $class ) ) :
+            throw("Please provide a string for your database key.");
+        endif;
+
+        $this->class = $class;
+
+        return $this;
+    }
+
+
+    public function add_cta( $message = "Thanks, I understand." , $link = '', $class = '')  {
+        if ( !empty( $link ) ) {
+            $link = ' href="' . $link . '" target="_blank"';
+        }
+
+        $html = '<a class="swp-notice-cta ' . $class . '" ' . $link . '>';
+            $html .= $message;
+        $html .= "</a>";
+
+        $this->actions .= $html;
+
+        return $this;
+    }
+
 
     public function render_HTML() {
-        $class = isset( $args['class_name'] ) ? $args['class_name'] : '';
 
-        $html = '<div class="swp-notice swp-dismiss-notice notice ' . $class . '" data-key=" <?php echo $this->key ?>">';
+        $html = '<div class="swp-notice swp-dismiss-notice ' . $this->class . '" data-key="' . $this->key . '">';
             $html .= '<p>' . $this->message . '</p>';
+            $html .= '<div class="swp-actions">';
+                $html .= $this->actions;
+            $html .= '</div>';
         $html .= '</div>';
 
         $this->html = $html;
+
         return $this;
+    }
+
+
+    public function get_HTML() {
+        if ( empty( $this->html ) ) :
+            $this->render_HTML();
+        endif;
+
+        return $this->html;
     }
 
 
     public function print_HTML() {
-        if ( empty( $this->html) ) :
+        if ( empty( $this->html ) ) :
             $this->render_HTML();
         endif;
 
         echo $this->html;
+
+        return $this;
+    }
+
+    public function ready() {
+        add_action( 'admin_notices', [$this, 'print_HTML'] );
+        add_action( 'swp_admin_notices', [$this, 'get_HTML'] );
+        add_action( 'wp_ajax_perma_dismiss', [ $this, 'perma_dismiss' ] );
+        add_action( 'wp_ajax_nopriv_perma_dismiss', [ $this, 'perma_dismiss' ] );
 
         return $this;
     }
