fdf2f669ea1fe55d8b9404e3344955e16ee23f40
Nicholas Cardot <nick@sitesketch101.com>
2018-06-07 04:51:12 -0500

    Updated to SWP_Notice

--- functions/includes/SWP_Notice.php
+++ functions/includes/SWP_Notice.php
@@ -1,30 +1,68 @@
 <?php
 
+/**
+ * SWP_Notice
+ *
+ * A class to control the creation and display of admin notices throughout the
+ * WordPress dashboard and on the Social Warfare settings page.
+ *
+ * @since  3.0.9 | 07 JUN 2018 | Created
+ * @access public
+ *
+ */
 class SWP_Notice {
-    public function __construct( $key, $message, $class = '' ) {
+
+
+	/**
+	 * The Magic __construct method
+	 *
+	 * This method will initialize our notice object and then add the necessary hooks to
+	 * allow it to be displayed and to be dismissed via admin-ajax.php.
+	 *
+	 * @since 3.0.9 | 07 JUN 2018 | Created
+	 * @param str $key     A unique key for this notice.
+	 * @param str $message The message for this notice
+	 *
+	 */
+    public function __construct( $key, $message ) {
         $this->init();
         $this->set_key( $key );
         $this->set_message( $message );
-        $this->set_class( $class );
-        $this->actions = [];
+        $this->actions = array();
 
-        add_action( 'admin_notices', [$this, 'print_HTML'] );
-        add_action( 'swp_admin_notices', [$this, 'get_HTML'] );
-        add_action( 'wp_ajax_perma_dismiss', [ $this, 'perma_dismiss' ] );
-        add_action( 'wp_ajax_nopriv_perma_dismiss', [ $this, 'perma_dismiss' ] );
+		// Add hooks to display our admin notices in the dashbaord and on our settings page.
+        add_action( 'admin_notices', array( $this, 'print_HTML' ) );
+        add_action( 'swp_admin_notices', array( $this, 'get_HTML' ) );
+
+		// Add a hook for permanently dismissing a notice via admin-ajax.php
+        add_action( 'wp_ajax_perma_dismiss', array( $this, 'perma_dismiss' ) );
+        add_action( 'wp_ajax_nopriv_perma_dismiss', array( $this, 'perma_dismiss' ) );
+
+		// Add a hook for temporarily dismissing a notice via admin-ajax.php
+        add_action( 'wp_ajax_temp_dismiss', array( $this, 'temp_dismiss' ) );
+        add_action( 'wp_ajax_nopriv_temp_dismiss', array( $this, 'temp_dismiss' ) );
+
     }
 
     public function init() {
-        $notices = get_option( 'social_warfare_dismissed_notices', [] );
+        $notices = get_option( 'social_warfare_dismissed_notices', array() );
 
         if ( [] === $notices ) {
-            update_option( 'social_warfare_dismissed_notices', [] );
+            update_option( 'social_warfare_dismissed_notices', array() );
         }
 
         $this->notices = $notices;
     }
 
 
+	/**
+	 * A method to determine if this notice should be displayed.
+	 *
+	 * @TODO   Check for timestamps and see if 30 days have elapsed.
+	 * @since  3.0.9 | 07 JUN 2018 | Created
+	 * @return bool true/false
+	 *
+	 */
     public function should_display_notice() {
         if ( empty( $this->notices[$this->key] ) ) {
 
@@ -35,12 +73,38 @@ class SWP_Notice {
     }
 
 
+	/**
+	 * A method to permanently dismiss notices via admin-ajax.php
+	 *
+	 * @since  3.0.9 | 07 JUN 2018 | Created
+	 * @param  none
+	 * @return none The respond from update_option is echoed.
+	 *
+	 */
     public function perma_dismiss() {
         $this->notices[$_POST['key']] = true;
 
         echo json_encode( update_option( 'social_warfare_dismissed_notices', $this->notices ) );
         wp_die();
     }
+
+
+	/**
+	 * A method to temporarily dismiss notices via admin-ajax.php
+	 *
+	 * @since  3.0.9 | 07 JUN 2018 | Created
+	 * @param  none
+	 * @return none The respond from update_option is echoed.
+	 *
+	 */
+	public function temp_dismiss() {
+		// TODO: Create a timestamp
+		$timestamp = '';
+		$this->notices[$_POST['key']] = $timestamp;
+
+		echo json_encode( update_option( 'social_warfare_dismissed_notices', $this->notices ) );
+		wp_die();
+	}
 
 
     public function set_message( $message ) {
@@ -64,18 +128,8 @@ class SWP_Notice {
         return $this;
     }
 
-    protected function set_class( $class ) {
-        if ( !is_string( $class ) ) :
-            throw("Please provide a string for your database key.");
-        endif;
 
-        $this->class = $class;
-
-        return $this;
-    }
-
-
-    public function add_cta( $action = '', $link = '', $class = '')  {
+    public function add_cta( $action = '', $link = '', $class = '' , $timeframe = 'permanent' )  {
         if ( '' === $action ) :
             $action = "Thanks, I understand.";
         endif;
@@ -84,10 +138,11 @@ class SWP_Notice {
             $link = ' href="' . $link . '" target="_blank"';
         endif;
 
-        $cta = [];
-        $cta['action'] = $action;
-        $cta['link'] = $link;
-        $cta['class'] = $class;
+        $cta              = array();
+        $cta['action']    = $action;
+        $cta['link']      = $link;
+        $cta['class']     = $class;
+		$cta['timeframe'] = $timeframe;
 
         $this->actions[] = $cta;
 
@@ -100,13 +155,13 @@ class SWP_Notice {
             $this->add_cta();
         endif;
 
-        $html = '<div class="swp-dismiss-notice notice ' . $this->class . '" data-key="' . $this->key . '">';
+        $html = '<div class="swp-dismiss-notice notice" data-key="' . $this->key . '">';
             $html .= '<p>' . $this->message . '</p>';
             $html .= '<p> - Warfare Plugins Team</p>';
             $html .= '<div class="swp-actions">';
 
                 foreach( $this->actions as $cta) {
-                    $html .= '<a class="swp-notice-cta ' . $cta['class'] . '" ' . $cta['link'] . '>';
+                    $html .= '<a class="swp-notice-cta ' . $cta['class'] . '" ' . $cta['link'] . ' data-timeframe="'.$cta['timeframe'].'">';
                         $html .= $cta['action'];
                     $html .= "</a>";
                 }
