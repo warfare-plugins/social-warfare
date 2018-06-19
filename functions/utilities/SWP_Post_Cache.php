2341cdc9208dddb244850bfdfcc7c3d01464c95d
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-06-19 17:21:14 -0400

    Created print_ajax_script

--- functions/utilities/SWP_Post_Cache.php
+++ functions/utilities/SWP_Post_Cache.php
@@ -121,7 +121,80 @@ class SWP_Post_Cache {
         global $Cache;
 
         if ( !$Cache->will_print_script ) {
-            
+            return;
+        }
+
+        ob_start();
+
+		?>
+        var within_timelimit;
+		swp_admin_ajax = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
+		var swp_buttons_exist = (document.getElementsByClassName( 'swp_social_panel' ).length > 0);
+        //* TODO remove the second true
+		if ( swp_buttons_exist ) {
+            if (!swp_buttons_exist) console.log("Forced the swp_cache_trigger. //* TODO: Remember to remove the 'true' in this condition.");
+			document.addEventListener('DOMContentLoaded', function() {
+				var swp_check_for_js = setInterval( function() {
+					if( 'undefined' !== typeof socialWarfarePlugin) {
+						clearInterval(swp_check_for_js);
+
+						<?php if( isset($_GET['swp_cache']) && 'rebuild' === $_GET['swp_cache'] ): ?>
+
+						var swp_cache_data = {
+							'action': 'swp_cache_trigger',
+							'post_id': <?php echo $info['postID']; ?>,
+		                    'timestamp': <?php echo time(); ?>,
+							'force':true
+						};
+
+						<?php else: ?>
+
+						var swp_cache_data = {
+							'action': 'swp_cache_trigger',
+							'post_id': <?php echo $info['postID']; ?>,
+		                    'timestamp': <?php echo time(); ?>
+						};
+
+						<?php endif; ?>
+
+    	                // if ( !swp_cache_data.timestamp ) { // error handling}
+    	                console.log( "Server Timestamp is " + swp_cache_data.timestamp );
+    	                var browser_date = Date.now();
+    	                if ( !browser_date )
+    	                    browser_date = new Date().getTime();
+    	                browser_date = Math.floor( browser_date / 1000 );
+    	                console.log( "Browser Timestamp is " + browser_date );
+    	                var elapsed_time = ( browser_date - swp_cache_data.timestamp );
+    	                if ( elapsed_time > 60 ) {
+    	                    console.log( "Elapsed time since server timestamp is greater than 60 seconds -- " + elapsed_time + "seconds" );
+    	                    within_timelimit = false;
+    	                } else {
+    	                    console.log( "Elapsed time since server timestamp is less than 60 seconds -- " + elapsed_time + "seconds"  );
+    	                    within_timelimit = true;
+    	                }
+
+    	                if ( within_timelimit === true ) {
+    									    jQuery.post( swp_admin_ajax, swp_cache_data, function( response ) {
+    										    console.log(response);
+    									    });
+
+    	                    socialWarfarePlugin.fetchShares();
+    	                }
+					}
+				} , 250 );
+			});
+
+			swp_post_id='<?php echo $info['postID']; ?>';
+			swp_post_url='<?php echo get_permalink(); ?>';
+			swp_post_recovery_url = '<?php echo $alternateURL; ?>';
+
+			//socialWarfarePlugin.fetchShares();
+		}
+
+		<?php
+        
+		$Cache->add_ajax_js( ob_get_clean() );
+
         }
     }
 }
