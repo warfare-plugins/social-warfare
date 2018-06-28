8ad76654a34236e9b49f1f231619126208a292d2
Nicholas Cardot <nick@sitesketch101.com>
2018-06-28 18:12:14 -0500

    Added debugging to notice class.

--- functions/utilities/SWP_Notice_Loader.php
+++ functions/utilities/SWP_Notice_Loader.php
@@ -32,6 +32,7 @@ class SWP_Notice_Loader {
     public function __construct() {
 		$this->activate_json_notices();
         $this->announce_stumble_upon_closing();
+		$this->debug();
     }
 
 
@@ -53,14 +54,14 @@ class SWP_Notice_Loader {
 		if( !is_array( $cache_data ) || empty($cache_data['notices']) ):
 			return;
 		endif;
-		
+
 		foreach( $cache_data['notices'] as $notice ):
 
 			$key     = $notice['key'];
 			$message = $notice['message'];
 			$ctas    = $notice['ctas'];
 
-			new SWP_Notice( $key, $message );
+			$this->notices[] = new SWP_Notice( $key, $message );
 
 		endforeach;
 	}
@@ -81,8 +82,26 @@ class SWP_Notice_Loader {
     private function announce_stumble_upon_closing() {
         $message = 'As of June 30th, 2018, StumbleUpon will no longer exist as a sharing platform. Instead, they are moving in with Mix. While this is exciting for Mix, <b>share counts will not be transferred, and Mix is not providing a share button or API. </b> You can read more about it <a href="https://help.stumbleupon.com/customer/en/portal/articles/2908172-transitioning-from-stumbleupon-to-mix" target="_blank">here</a>.';
 
-        $notice = new SWP_Notice( 'stumble_upon_closed', $message, 'notice-info' );
+        $this->notices[] = new SWP_Notice( 'stumble_upon_closed', $message, 'notice-info' );
     }
 
+
+	/**
+	 * A function for debugging this class.
+	 *
+	 * All notices are stored in the $this->notices as an array of notice
+	 * objects. Since this is the last method called, all notices should be
+	 * present in the $this object for review. 
+	 *
+	 * @since  3.1.0 | 28 JUN 2018 | Created
+	 * @param  void
+	 * @return void
+	 *
+	 */
+	private function debug() {
+		if( true === _swp_debug( 'notices' ) ):
+			var_dump($this);
+		endif;
+	}
 
 }
