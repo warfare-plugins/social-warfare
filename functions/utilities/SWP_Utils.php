6f8fcaf5740c14ee91a38d87d07eee635c6cfab4
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-08-14 14:35:46 -0400

    Created SWP_Utils. #594

---
+++ functions/utilities/SWP_Utils.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * SWP_Utils
+ *
+ * A collection of utility functions.
+ *
+ * All of the methods should be static. The class serves as tidy container
+ * for various utility functions.
+ *
+ * The constructor serves only to set up hooks and filters.
+ *
+ * @package   SocialWarfare\Utilities
+ * @copyright Copyright (c) 2018, Warfare Plugins, LLC
+ * @license   GPL-3.0+
+ * @since     3.3.0 | 14 AUG 2018 | Created
+ * @access    public
+ *
+ */
+class SWP_Utils {
+    public static function get_option(){}
+
+    public static function store_settings(){}
+
+    public static function socialWarfare(){}
+
+    public static function social_warfare(){}
+
+    public static function kilomega(){}
+
+    public static function get_excerpt_by_id(){}
+
+    public static function debug(){}
+
+    public static function convert_smart_quotes(){}
+
+    public static function get_post_types(){}
+
+    public static function remove_screen_options(){}
+
+    public static function get_site_url(){}
+
+
+}
