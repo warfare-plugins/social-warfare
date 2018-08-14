fcb66e73759d2ff7ab1294a007937ef12ef97ad1
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-08-14 15:21:15 -0400

    Created functions.php. #594

---
+++ functions/utilities/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file serves to hold global functions made available to developers
+ * by Social Warfare.
+ *
+ * Any actual plugin functionality needs to exist as a class method somewhere else.
+ *
+ * @since  3.3.0 | 14 AUG 2018 | Created file.
+ *
+ */
+
+function socialWarfare( $content = false, $where = 'default', $echo = true ) {
+    social_warfare( array( 'content' => $content, 'where' => $where, 'echo' => $echo ) );
+}
+
+function social_warfare( $args = array() ) {
+    $buttons_panel = new SWP_Buttons_Panel( $args );
+    return $buttons_panel->render_HTML();
+}
