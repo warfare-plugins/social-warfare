4d2ba60c8a2d2426979b845328ec4a86a7630874
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-08-17 10:54:13 -0400

    Revert "Added prefix to Puc_v4_Factory."
    This reverts commit 6979fa6b948272c768f5ea5045dfee69fe8253e2.

--- functions/plugin-update-checker/plugin-update-checker.php
+++ functions/plugin-update-checker/plugin-update-checker.php
@@ -13,12 +13,12 @@ require dirname(__FILE__) . '/Puc/v4p4/Autoloader.php';
 new Puc_v4p4_Autoloader();
 
 //Register classes defined in this file with the factory.
-SWP_Puc_v4_Factory::addVersion('Plugin_UpdateChecker', 'SWP_Puc_v4p4_Plugin_UpdateChecker', '4.4');
-SWP_Puc_v4_Factory::addVersion('Theme_UpdateChecker', 'Puc_v4p4_Theme_UpdateChecker', '4.4');
+Puc_v4_Factory::addVersion('Plugin_UpdateChecker', 'SWP_Puc_v4p4_Plugin_UpdateChecker', '4.4');
+Puc_v4_Factory::addVersion('Theme_UpdateChecker', 'Puc_v4p4_Theme_UpdateChecker', '4.4');
 
-SWP_Puc_v4_Factory::addVersion('Vcs_PluginUpdateChecker', 'Puc_v4p4_Vcs_PluginUpdateChecker', '4.4');
-SWP_Puc_v4_Factory::addVersion('Vcs_ThemeUpdateChecker', 'Puc_v4p4_Vcs_ThemeUpdateChecker', '4.4');
+Puc_v4_Factory::addVersion('Vcs_PluginUpdateChecker', 'Puc_v4p4_Vcs_PluginUpdateChecker', '4.4');
+Puc_v4_Factory::addVersion('Vcs_ThemeUpdateChecker', 'Puc_v4p4_Vcs_ThemeUpdateChecker', '4.4');
 
-SWP_Puc_v4_Factory::addVersion('GitHubApi', 'Puc_v4p4_Vcs_GitHubApi', '4.4');
-SWP_Puc_v4_Factory::addVersion('BitBucketApi', 'Puc_v4p4_Vcs_BitBucketApi', '4.4');
-SWP_Puc_v4_Factory::addVersion('GitLabApi', 'Puc_v4p4_Vcs_GitLabApi', '4.4');
+Puc_v4_Factory::addVersion('GitHubApi', 'Puc_v4p4_Vcs_GitHubApi', '4.4');
+Puc_v4_Factory::addVersion('BitBucketApi', 'Puc_v4p4_Vcs_BitBucketApi', '4.4');
+Puc_v4_Factory::addVersion('GitLabApi', 'Puc_v4p4_Vcs_GitLabApi', '4.4');
