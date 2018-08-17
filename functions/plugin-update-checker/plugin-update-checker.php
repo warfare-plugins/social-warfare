4ff0f4d329ecbf03caebabb622e4ee63abae44bd
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-08-17 11:05:19 -0400

    More prefixing.

--- functions/plugin-update-checker/plugin-update-checker.php
+++ functions/plugin-update-checker/plugin-update-checker.php
@@ -16,7 +16,7 @@ new Puc_v4p4_Autoloader();
 SWP_Puc_v4_Factory::addVersion('Plugin_UpdateChecker', 'SWP_Puc_v4p4_Plugin_UpdateChecker', '4.4');
 SWP_Puc_v4_Factory::addVersion('Theme_UpdateChecker', 'Puc_v4p4_Theme_UpdateChecker', '4.4');
 
-SWP_Puc_v4_Factory::addVersion('Vcs_PluginUpdateChecker', 'Puc_v4p4_Vcs_PluginUpdateChecker', '4.4');
+SWP_Puc_v4_Factory::addVersion('Vcs_PluginUpdateChecker', 'SWP_Puc_v4p4_Vcs_PluginUpdateChecker', '4.4');
 SWP_Puc_v4_Factory::addVersion('Vcs_ThemeUpdateChecker', 'Puc_v4p4_Vcs_ThemeUpdateChecker', '4.4');
 
 SWP_Puc_v4_Factory::addVersion('GitHubApi', 'Puc_v4p4_Vcs_GitHubApi', '4.4');
