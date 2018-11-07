ee96d6657559fe31d2a8c135389d81bf3a679c12
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-11-07 17:06:56 -0500

    Updating scss files and organization.

--- assets/js/post-editor/src/block/click-to-tweet.js
+++ assets/js/post-editor/src/block/click-to-tweet.js
@@ -1,6 +1,6 @@
 
+import '../common.scss';
 import './style.scss';
-import './editor.scss';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
