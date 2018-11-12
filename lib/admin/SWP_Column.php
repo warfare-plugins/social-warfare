fb4b16a58c7b199c449486ebe74f2bf44a81d243
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-11-12 11:54:18 -0500

    Fixed a typeo.

--- lib/admin/SWP_Column.php
+++ lib/admin/SWP_Column.php
@@ -82,7 +82,7 @@ class SWP_Column {
 		// Get the share count, format it, echo it to the screen.
  		$count = get_post_meta( $post_ID , '_total_shares' , true );
 		if( !empty( $count ) ) {
- 			echo SWP_Utility::kilometa( $count );
+ 			echo SWP_Utility::kilomega( $count );
 		}
 
 		echo 0;
