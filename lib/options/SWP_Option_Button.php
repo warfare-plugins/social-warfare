8251abb58eaef629dded87ae8baf02a08559b1d1
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-03 16:17:04 -0500

    Created SWP_Option_Button.

---
+++ lib/options/SWP_Option_Button.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * A button with a CTA. The immediate use case is for network authorizations.
+ *
+ */
+class SWP_Option_Button extends SWP_Option {
+
+
+	/**
+	* Default
+	*
+	* The default value for this input type="checkbox".
+	*
+	* @var bool $default
+	*
+	*/
+	public $default = true;
+
+
+	/**
+	* The required constructor for PHP classes.
+	*
+	* @param string $name The display name for the toggle.
+	* @param string $key The database key for the user setting.
+	*/
+	public function __construct( $name, $key, $link ) {
+		parent::__construct( $name, $key );
+		$this->default = true;
+	}
+
+
+	public function register_available_values( $values ) {
+		$values[$this->key] = array(
+			'type'      => 'boolean',
+			'values'    => array( true, false )
+		);
+
+		return $values;
+	}
+
+
+	/**
+	* Creates ready-to-print HTML for the checkbox/toggle module.
+	*
+	* @return SWP_Option_Toggle $this The calling object, for method chaining.
+	*/
+	public function render_HTML() {
+		$checked = $status === 'on' ? ' checked ' : '';
+
+		$html = '<div class="sw-grid ' . $this->parent_size . ' sw-fit sw-option-container ' . $this->key . '_wrapper" ';
+		$html .= $this->render_dependency();
+		$html .= $this->render_premium();
+		$html .= '>';
+
+			$html .= '<div class="sw-grid ' . $this->size . '">';
+				$html .= '<p class="">' . $this->name . '</p>';
+			$html .= '</div>';
+
+			$html .= '<div class="sw-grid ' . $this->size . '">';
+				$html .= '<button field="#' . $this->key . '">' . $this->name . '</button>';
+			$html .= '</div>';
+
+		$html .= '</div>';
+
+		$this->html = $html;
+
+		return $html;
+	}
+
+
+	/**
+	* Override parent method to make this boolean-specific.
+	*
+	* @param boolean $value The boolean value to set as default.
+	* @return SWP_Option_Toggle $this The calling object, for method chaining.
+	*/
+	public function set_default( $value ) {
+		if ( !is_bool( $value ) ||  !isset( $value ) ) {
+			$this->_throw( 'Please provide a default value as a boolean.' );
+		}
+
+		return parent::set_default( $value );
+	}
+}
