f4e2238557e2fc92da74bd60e9c63d1441db4fda
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-01-10 16:40:02 -0500

    Moved require statement to Main Class.

--- social-warfare.php
+++ social-warfare.php
@@ -30,10 +30,12 @@ define( 'SWP_STORE_URL', 'https://warfareplugins.com' );
  *
  */
 if ( file_exists( SWP_PLUGIN_DIR . '/lib/legacy/update-checker.php') ) :
-    require_once SWP_PLUGIN_DIR . '/lib/legacy/update-checker.php';
+	require_once SWP_PLUGIN_DIR . '/lib/legacy/update-checker.php';
 endif;
 add_filter('the_excerpt', 'do_shortcode', 1);
 
 // Load the main Social_Warfare class and fire up the plugin.
 require_once SWP_PLUGIN_DIR . '/lib/Social_Warfare.php';
+
+
 new Social_Warfare();
