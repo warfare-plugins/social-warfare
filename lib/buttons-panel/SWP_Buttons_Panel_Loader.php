abf595749240989048d7c07ebc570f40845ea6ec
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-02-05 13:08:06 -0500

    Added check for is_object; spaces to tabs.

--- lib/buttons-panel/SWP_Buttons_Panel_Loader.php
+++ lib/buttons-panel/SWP_Buttons_Panel_Loader.php
@@ -26,7 +26,7 @@ class SWP_Buttons_panel_Loader {
 	 * @var array
 	 *
 	 */
-    public $already_printed;
+	public $already_printed;
 
 
 	/**
@@ -51,16 +51,16 @@ class SWP_Buttons_panel_Loader {
 	public $content_loaded = false;
 
 
-    /**
-     * The class constructor.
-     *
-     * @since  3.1.0 | Changed priority for wp_footer. Makes the buttons loads
-     *                 This post data instead of data in the loop.
-     * @param  void
-     * @return void
-     *
-     */
-    public function __construct() {
+	/**
+	 * The class constructor.
+	 *
+	 * @since  3.1.0 | Changed priority for wp_footer. Makes the buttons loads
+	 *                 This post data instead of data in the loop.
+	 * @param  void
+	 * @return void
+	 *
+	 */
+	public function __construct() {
 
 
 		/**
@@ -69,114 +69,114 @@ class SWP_Buttons_panel_Loader {
 		 * the_content filter on the same post more than once.
 		 *
 		 */
-        global $swp_already_print;
+		global $swp_already_print;
 
 		// The global array of the user-selected options.
-        global $swp_user_options;
+		global $swp_user_options;
 
 		// Declare variable as array if not already done so.
-        if ( !is_array( $swp_already_print ) ) {
-            $swp_already_print = array();
-        }
+		if ( !is_array( $swp_already_print ) ) {
+			$swp_already_print = array();
+		}
 
 		// Move these two globals into local properties.
-        $this->already_printed = $swp_already_print;
-        $this->options = $swp_user_options;
+		$this->already_printed = $swp_already_print;
+		$this->options = $swp_user_options;
 
-        // Hook into the template_redirect so that is_singular() conditionals will be ready
-        add_action( 'template_redirect', array( $this, 'activate_buttons' ) );
-        add_action( 'wp_footer', array( $this, 'floating_buttons' ) , 20 );
+		// Hook into the template_redirect so that is_singular() conditionals will be ready
+		add_action( 'template_redirect', array( $this, 'activate_buttons' ) );
+		add_action( 'wp_footer', array( $this, 'floating_buttons' ) , 20 );
 		add_filter( 'the_content', array( $this, 'add_static_panel_fallback_content' ) , 20 );
 		add_action( 'wp_footer', array( $this, 'add_static_panel_fallback_footer' ) , 20 );
-    }
+	}
 
 
-    /**
-     * A function to add the buttons
-     *
-     * @since  2.1.4 | 01 JAN 2017 | Created
-     * @since  3.0.6 | 14 MAY 2018 | Added second filter for the_content.
-     * @param  void
-     * @return void
-     *
-     */
-    public function activate_buttons() {
+	/**
+	 * A function to add the buttons
+	 *
+	 * @since  2.1.4 | 01 JAN 2017 | Created
+	 * @since  3.0.6 | 14 MAY 2018 | Added second filter for the_content.
+	 * @param  void
+	 * @return void
+	 *
+	 */
+	public function activate_buttons() {
 
 		// Bail if we're in the presence of a known conflict without a fix.
-        if ( Social_Warfare::has_plugin_conflict() ) {
-            return;
-        }
+		if ( Social_Warfare::has_plugin_conflict() ) {
+			return;
+		}
 
-    	// Only hook into the_content filter if is_singular() is true or
-    	// they don't use excerpts on the archive pages.
-        if( is_singular() || true === SWP_Utility::get_option( 'full_content' ) ) {
-            add_filter( 'the_content', array( $this, 'social_warfare_wrapper' ) , 20 );
-            add_filter( 'the_content', array( $this, 'add_content_locator' ), 20);
-        }
+		// Only hook into the_content filter if is_singular() is true or
+		// they don't use excerpts on the archive pages.
+		if( is_singular() || true === SWP_Utility::get_option( 'full_content' ) ) {
+			add_filter( 'the_content', array( $this, 'social_warfare_wrapper' ) , 20 );
+			add_filter( 'the_content', array( $this, 'add_content_locator' ), 20);
+		}
 
 		// If we're not on is_singlular, we'll hook into the excerpt.
-        if ( !is_singular() && false === SWP_Utility::get_option( 'full_content' ) ) {
-    		add_filter( 'the_excerpt', array( $this, 'social_warfare_wrapper' ) );
-        }
-    }
+		if ( !is_singular() && false === SWP_Utility::get_option( 'full_content' ) ) {
+			add_filter( 'the_excerpt', array( $this, 'social_warfare_wrapper' ) );
+		}
+	}
 
 
-    /**
-     * Add the content locator div.
-     *
-     * Inserts the empty div for locating Pin images (with javascript). We only
-     * add this to the content if we need it.
-     *
-     * 1. If the Pinit Image Hover Buttons are active we'll use this locator div
-     * to ensure that we are only adding the "save" button to images that are in
-     * the content area.
-     *
-     * 2. If the "float_before_content" otpion is turned off, we'll use this
-     * locator div to determine where the content is and then not display the
-     * buttons panel unless we are past the top of the content area.
-     *
-     * @since  3.0.6 | 14 MAY 2018 | Created the method.
-     * @since  3.4.0 | 19 SEP 2018 | Added check for pinit_toggle option.
-     * @since  3.4.2 | 11 DEC 2018 | Added check for float_before_content option. 
-     * @param  string $content The WordPress content passed via filter.
-     * @return string $content The modified string of content.
-     *
-     */
-    public function add_content_locator( $content ) {
+	/**
+	 * Add the content locator div.
+	 *
+	 * Inserts the empty div for locating Pin images (with javascript). We only
+	 * add this to the content if we need it.
+	 *
+	 * 1. If the Pinit Image Hover Buttons are active we'll use this locator div
+	 * to ensure that we are only adding the "save" button to images that are in
+	 * the content area.
+	 *
+	 * 2. If the "float_before_content" otpion is turned off, we'll use this
+	 * locator div to determine where the content is and then not display the
+	 * buttons panel unless we are past the top of the content area.
+	 *
+	 * @since  3.0.6 | 14 MAY 2018 | Created the method.
+	 * @since  3.4.0 | 19 SEP 2018 | Added check for pinit_toggle option.
+	 * @since  3.4.2 | 11 DEC 2018 | Added check for float_before_content option.
+	 * @param  string $content The WordPress content passed via filter.
+	 * @return string $content The modified string of content.
+	 *
+	 */
+	public function add_content_locator( $content ) {
 		$pinit_toggle         = SWP_Utility::get_option( 'pinit_toggle' );
 		$float_before_content = SWP_Utility::get_option( 'float_before_content' );
 
 		if( $pinit_toggle || !$float_before_content ) {
-        	$content .= '<div class="swp-content-locator"></div>';
+			$content .= '<div class="swp-content-locator"></div>';
 		}
 
-        return $content;
-    }
+		return $content;
+	}
 
 
-    /**
-    * A wrapper function for adding the buttons, content, or excerpt.
-    *
-    * @since  1.0.0
-    * @param  string $content The content.
-    * @return string $content The modified content
-    * @todo   Why is the $content passed to both the instantator and the method?
-    *
-    */
-    public function social_warfare_wrapper( $content ) {
+	/**
+	* A wrapper function for adding the buttons, content, or excerpt.
+	*
+	* @since  1.0.0
+	* @param  string $content The content.
+	* @return string $content The modified content
+	* @todo   Why is the $content passed to both the instantator and the method?
+	*
+	*/
+	public function social_warfare_wrapper( $content ) {
 
 		// The global WordPress post object.
-        global $post;
+		global $post;
 
-      	// Ensure it's not an embedded post
-      	if ( is_singular() && $post->ID !== get_queried_object_id() ) {
+		  // Ensure it's not an embedded post
+		  if ( is_singular() && $post->ID !== get_queried_object_id() ) {
 			return $content;
-      	}
+		  }
 
-        // Pass the content to the buttons constructor to place them inside.
-    	$buttons_panel = new SWP_Buttons_Panel( array( 'content' => $content ) );
-        return $buttons_panel->render_html();
-    }
+		// Pass the content to the buttons constructor to place them inside.
+		$buttons_panel = new SWP_Buttons_Panel( array( 'content' => $content ) );
+		return $buttons_panel->render_html();
+	}
 
 
 	/**
@@ -187,26 +187,26 @@ class SWP_Buttons_panel_Loader {
 	 * @return void
 	 *
 	 */
-    function floating_buttons() {
+	function floating_buttons() {
 
 		// Bail if we're in the presence of a known conflict with no fix.
-        if ( Social_Warfare::has_plugin_conflict() ) {
-            return;
-        }
+		if ( Social_Warfare::has_plugin_conflict() ) {
+			return;
+		}
 
 		// Instantiate a new Buttons Panel.
-        $side_panel = new SWP_Buttons_Panel_Side( array( 'content' => "" ) );
+		$side_panel = new SWP_Buttons_Panel_Side( array( 'content' => "" ) );
 
 		// Determine if the floating buttons are not supposed to print.
-        $location = $side_panel->get_float_location();
-        if ( 'none' === $location || 'ignore' === $location ) {
-            return;
-        }
+		$location = $side_panel->get_float_location();
+		if ( 'none' === $location || 'ignore' === $location ) {
+			return;
+		}
 
 		// Render the html to output to the screen.
-        echo $side_panel->render_html();
+		echo $side_panel->render_html();
 
-    }
+	}
 
 
 	/**
@@ -261,26 +261,26 @@ class SWP_Buttons_panel_Loader {
 	}
 
 
-    /**
-     * When floatingHorizontal buttons are desired, but not staticHorizontal
-     * exists, we need to create a staticHorizontal so the floaters have
-     * a target to clone.
-     *
-     * @since  3.4.0 | 25 OCT 2018 | Created.
-     * @param  void
-     * @return void The rendered HTML is echoed to the screen.
-     *
-     */
+	/**
+	 * When floatingHorizontal buttons are desired, but not staticHorizontal
+	 * exists, we need to create a staticHorizontal so the floaters have
+	 * a target to clone.
+	 *
+	 * @since  3.4.0 | 25 OCT 2018 | Created.
+	 * @param  void
+	 * @return void The rendered HTML is echoed to the screen.
+	 *
+	 */
 	public function generate_static_panel_fallback( $content = '' ) {
 		global $post;
 
 
-        /**
-         * If all the checks above get passed, then we'll go ahead and create a
-         * static horizontal buttons panel, wrap it in a wrapper to make it
-         * invisible, and echo it to the screen.
-         *
-         */
+		/**
+		 * If all the checks above get passed, then we'll go ahead and create a
+		 * static horizontal buttons panel, wrap it in a wrapper to make it
+		 * invisible, and echo it to the screen.
+		 *
+		 */
 		$staticHorizontal = new SWP_Buttons_Panel();
 		$html  = '<div class="swp-hidden-panel-wrap" style="display: none; visibility: collapse; opacity: 0">';
 		$html .= $staticHorizontal->render_html();
@@ -300,6 +300,10 @@ class SWP_Buttons_panel_Loader {
 	 */
 	public function should_float_fallback_display() {
 		global $post;
+
+		if ( !is_object( $post ) ) {
+			return false;
+		}
 
 
 		/**
@@ -379,18 +383,18 @@ class SWP_Buttons_panel_Loader {
 	}
 
 
-    /**
-     * The main social_warfare function used to create the buttons.
-     *
-     * @since  3.0.0 | 01 MAR 2018 | A class based method created which clones
-     *                               the public facing function.
-     * @param  array $array An array of options and information to pass into the
-     *                      buttons function.
-     * @return string       The html for a panel of buttons.
-     *
-     */
-    public static function social_warfare( $args = array() ) {
-        $Buttons_Panel = new SWP_Buttons_Panel( $args );
-    	echo $Buttons_Panel->render_html();
-    }
+	/**
+	 * The main social_warfare function used to create the buttons.
+	 *
+	 * @since  3.0.0 | 01 MAR 2018 | A class based method created which clones
+	 *                               the public facing function.
+	 * @param  array $array An array of options and information to pass into the
+	 *                      buttons function.
+	 * @return string       The html for a panel of buttons.
+	 *
+	 */
+	public static function social_warfare( $args = array() ) {
+		$Buttons_Panel = new SWP_Buttons_Panel( $args );
+		echo $Buttons_Panel->render_html();
+	}
 }
