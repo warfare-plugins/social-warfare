49890e97ee9d01abf77949e3c38fff295fd8ec78
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-04-22 11:22:44 -0400

    Updated facebook request process.
    Co-Authored-By: Nicholas Cardot <nicholas-cardot@users.noreply.github.com>

--- lib/social-networks/SWP_Facebook.php
+++ lib/social-networks/SWP_Facebook.php
@@ -11,6 +11,15 @@
  * @since     1.0.0 | Unknown     | CREATED
  * @since     2.2.4 | 02 MAY 2017 | Refactored functions & updated docblocking
  * @since     3.0.0 | 05 APR 2018 | Rebuilt into a class-based system.
+ * @since     3.6.0 | 22 APR 2018 | Removed all Javascript related functions for
+ *                                  fetching share counts. This includes:
+ *                                      register_cache_processes()
+ *                                      add_facebook_footer_hook()
+ *                                      print_facebook_script()
+ *                                      facebook_shares_update()
+ *                                 Shares are now fetched using the same two
+ *                                 method process that are used by all other
+ *                                 social networks in the plugin.
  *
  */
 class SWP_Facebook extends SWP_Social_Network {
@@ -41,11 +50,6 @@ class SWP_Facebook extends SWP_Social_Network {
 		$this->base_share_url = 'https://www.facebook.com/share.php?u=';
 
 		$this->init_social_network();
-
-        if( true === $this->is_active() ):
-    		$this->register_cache_processes();
-        endif;
-
 	}
 
 
@@ -53,13 +57,15 @@ class SWP_Facebook extends SWP_Social_Network {
 	 * Generate the API Share Count Request URL
 	 *
 	 * @since  1.0.0 | 06 APR 2018 | Created
+	 * @since  3.6.0 | 22 APR 2019 | Updated API to v3.2.
 	 * @access public
 	 * @param  string $url The permalink of the page or post for which to fetch share counts
 	 * @return string $request_url The complete URL to be used to access share counts via the API
 	 *
 	 */
 	public function get_api_link( $url ) {
-		return 'https://graph.facebook.com/?fields=og_object{likes.summary(true).limit(0)},share&id=' . $url;
+		$access_token = base64_decode('MTc5NjYwNzk4Nzc0Mjk2fGZld2FfS0VPUzBwZWxzcFBPZndfanFsanFUaw==');
+		return 'https://graph.facebook.com/v3.2/?id='.$url.'&fields=engagement&access_token=' . $access_token;
 	}
 
 
@@ -67,132 +73,29 @@ class SWP_Facebook extends SWP_Social_Network {
 	 * Parse the response to get the share count
 	 *
 	 * @since  1.0.0 | 06 APR 2018 | Created
+	 * @since  3.6.0 | 22 APR 2019 | Updated to parse API v. 3.2.
 	 * @access public
-	 * @param  string $response The raw response returned from the API request
-	 * @return int $total_activity The number of shares reported from the API
+	 * @param  string  $response The raw response returned from the API request
+	 * @return integer $total_activity The number of shares reported from the API
 	 *
 	 */
 	public function parse_api_response( $response ) {
+
+		// JSON decode the response.
 		$formatted_response = json_decode( $response , true);
 
-		if( !empty( $formatted_response['og_object'] ) ) {
-			$likes = $formatted_response['og_object']['likes']['summary']['total_count'];
+		// Parse the response to get integers.
+		if( !empty( $formatted_response['engagement'] ) ) {
+			$reaction_count = $formatted_response['engagement']['reaction_count'];
+			$comment_count  = $formatted_response['engagement']['comment_count'];
+			$share_count    = $formatted_response['engagement']['share_count'];
 		} else {
-			$likes = 0;
+			$reaction_count = $comment_count = $share_count = 0;
 		}
 
-		if( !empty( $formatted_response['share'] ) ){
-			$comments = $formatted_response['share']['comment_count'];
-			$shares = $formatted_response['share']['share_count'];
-		} else {
-			$comments = 0;
-			$shares = 0;
-		}
-
-		$total = $likes + $comments + $shares;
+		// Add up the integers and return the total.
+		$total = $reaction_count + $comment_count + $share_count;
 		return $total;
-	}
-
-
-	/**
-	 * Register Cache Processes
-	 *
-	 * This method registered the processes that will need to be run during the
-	 * cache rebuild process. The new caching class (codenames neo-advanced cache
-	 * method) allows us to hook in functions that will run during the cache
-	 * rebuild process by hooking into the swp_cache_rebuild hook.
-	 *
-	 * @since  3.1.0 | 26 JUN 2018 | Created
-	 * @param  void
-	 * @return void
-	 *
-	 */
-	private function register_cache_processes() {
-
-		add_action( 'swp_cache_rebuild', array( $this, 'add_facebook_footer_hook' ), 10, 1 );
-		add_action( 'wp_ajax_swp_facebook_shares_update', array( $this, 'facebook_shares_update' ) );
-		add_action( 'wp_ajax_nopriv_swp_facebook_shares_update', array( $this, 'facebook_shares_update' ) );
-	}
-
-
-	/**
-	 * A function to add the Facebook updater to the footer hook.
-	 *
-	 * This is a standalone method because we only want to hook into the footer
-	 * and display the script during the cache rebuild process.
-	 *
-	 * @since  3.1.0 | 25 JUN 2018 | Created
-	 * @param  void
-	 * @return void
-	 *
-	 */
-	public function add_facebook_footer_hook( $post_id ) {
-        $this->post_id = $post_id;
-		add_action( 'wp_footer', array( $this, 'print_facebook_script' ) );
-	}
-
-
-	/**
-	 * Output the AJAX/JS for updating Facebook share counts.
-	 *
-	 * @since  3.1.0 | 25 JUN 2018 | Created
-	 * @param  void
-	 * @return void Output is printed directly to the screen.
-	 *
-	 */
-	public function print_facebook_script() {
-
-		if ( true === SWP_Utility::get_option( 'recover_shares' ) ) {
-			$alternateURL = SWP_Permalink::get_alt_permalink( $this->post_id );
-		} else {
-			$alternateURL = false;
-		}
-
-		echo '<script type="text/javascript">
-			document.addEventListener("DOMContentLoaded", function() {
-				var swpButtonsExist = document.getElementsByClassName( "swp_social_panel" ).length > 0;
-				if (swpButtonsExist) {
-					swp_admin_ajax = "' . admin_url( 'admin-ajax.php' ) . '";
-					swp_post_id=' . (int) $this->post_id . ';
-					swp_post_url= "' . get_permalink() . '";
-					swp_post_recovery_url = "' . $alternateURL . '";
-					socialWarfare.fetchFacebookShares();
-				}
-			});
-			</script>
-		';
-	}
-
-
-	/**
-	 * Process the Facebook shares response via admin-ajax.php.
-	 *
-	 * The object will be instantiated by the Cache_Loader class and it will
-	 * then call this method from there.
-	 *
-	 * @since  3.1.0 | 25 JUN 2018 | Created
-	 * @param  void
-	 * @return void
-	 *
-	 */
-	public function facebook_shares_update() {
-		global $swp_user_options;
-
-		if (!is_numeric( $_POST['share_counts'] ) || !is_numeric( $_POST['post_id'] ) ) {
-			wp_die();
-		}
-
-		$activity = (int) $_POST['share_counts'];
-		$post_id  = (int) $_POST['post_id'];
-
-		$previous_activity = get_post_meta( $post_id, '_facebook_shares', true );
-
-		if ( $activity > $previous_activity || true === SWP_Utility::debug('force_new_shares') ) :
-			delete_post_meta( $post_id, '_facebook_shares' );
-			update_post_meta( $post_id, '_facebook_shares', $activity );
-		endif;
-
-		wp_die();
 	}
 
 }
