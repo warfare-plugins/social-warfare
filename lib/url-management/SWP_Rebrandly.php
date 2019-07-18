3f03c609bf04f7053750d5bf4226dd843f458683
Nicholas Z. Cardot <nick@sitesketch101.com>
2019-07-17 22:13:28 -0500

    Created the placeholder Rebrandly class.

--- lib/url-management/SWP_Rebrandly.php
+++ lib/url-management/SWP_Rebrandly.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+
+/**
+ * SWP_Rebrandly
+ *
+ * This class will control all of the link shortening functionality for the
+ * Rebrandly integration.
+ *
+ * @since 4.0.0 | 17 JUL 2019 | Created
+ */
+class SWP_Rebrandly {
+	
+}
