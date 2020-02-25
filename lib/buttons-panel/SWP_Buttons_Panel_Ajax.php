16b3f1d319d23f3d52b5d312988c6aabe2b9cdf6
Nicholas Z. Cardot <nick@sitesketch101.com>
2020-02-25 01:08:08 -0600

    Documenting the admin-ajax handler for the "More" html. #738

--- lib/buttons-panel/SWP_Buttons_Panel_Ajax.php
+++ lib/buttons-panel/SWP_Buttons_Panel_Ajax.php
@@ -1,8 +1,31 @@
 <?php
 
+/**
+ * SWP_Buttons_Panel_Ajax
+ *
+ * This class will register the action needed to allow calls to admin ajax that
+ * will fetch the buttons panels containing all of the available buttons. This
+ * will be used for creating the lightbox popup that will be populated with all
+ * sharing buttons and will be activated when the "More" button is clicked on.
+ *
+ * @since  4.0.0 | 25 FEB 2020 | Created
+ *
+ */
 class SWP_Buttons_Panel_Ajax {
 
 
+	/**
+	 * The Magic Constructor
+	 *
+	 * This will isntantiate the class and do nothing more than register our
+	 * method as a callback handler so that it will intercept and handle all of
+	 * the calls to admin-ajax.php while using the swp_buttons_panel action.
+	 *
+	 * @since  4.0.0 | 25 FEB 2020 | Created
+	 * @param  void
+	 * @return void
+	 *
+	 */
 	public function __construct() {
 
 		// Register our method to fire when the hook is called.
@@ -11,15 +34,43 @@ class SWP_Buttons_Panel_Ajax {
 	}
 
 
+	/**
+	 * The build_buttons_panels() method is a declarative method that simply
+	 * walks through all of the helper methods that build out the html for the
+	 * buttons panels that will be generated into html.
+	 *
+	 * Note: This must be public to be used as a hook callback.
+	 *
+	 * @since  4.0.0 | 25 FEB 2020 | Created
+	 * @access public
+	 * @param  void
+	 * @return void
+	 *
+	 */
 	public function build_buttons_panels() {
 		$this->establish_available_buttons();
 		$this->generate_buttons_panel_html();
 		$this->generate_lightbox_container();
 
+		// This must be included to properly close out a call to admin-ajax.php.
 		wp_die();
 	}
 
+
+	/**
+	 * The establish_available_buttons() method will use the global array
+	 * containing every registered social network in the plugin. It will divide
+	 * it into arrays containing 4 networks each. These will be used to generate
+	 * the buttons panel for each row.
+	 *
+	 * @since  4.0.0 | 25 FEB 2020 | Created
+	 * @param  void
+	 * @return void
+	 *
+	 */
 	private function establish_available_buttons() {
+
+		// Pull the global array of networks into a local variable.
 		global $swp_social_networks;
 		$networks = $swp_social_networks;
 
@@ -28,16 +79,19 @@ class SWP_Buttons_Panel_Ajax {
 			return strcmp($a->name, $b->name);
 		});
 
+		// Loope through and sort them into stacks of 4 networks.
 		$group = 0;
 		$i = 0;
 		foreach( $networks as $network ) {
 
+			// Skip the "More" button since we're already inside it's options.
 			if( 'more' === $network->key ) {
 				continue;
 			}
 
 			$sorted_networks[$group][] = $network;
 
+			// Iterate our counters.
 			$i++;
 			if( $i % 4 === 0 ) {
 				$group++;
@@ -48,15 +102,31 @@ class SWP_Buttons_Panel_Ajax {
 		$this->network_groups = $sorted_networks;
 	}
 
+
+	/**
+	 * The generate_buttons_panel_html() will use the array of available networks,
+	 * loop through the networks, and instantiate buttons_panel objects, and use
+	 * their render_html() methods to create the html for each panel.
+	 *
+	 * @since  4.0.0 | 25 FEB 2020 | Created
+	 * @param  void
+	 * @return void
+	 *
+	 */
 	private function generate_buttons_panel_html() {
 		$buttons = '';
 
+		// Loop through each network group containing 4 networks. Each group will
+		// be a row of buttons.
 		foreach( $this->network_groups as $network_group ) {
+
+			// Loop through each network within this group and add it to our args list.
 			$networks = '';
 			foreach( $network_group as $network ) {
 				$networks .= ',' . $network->key;
 			}
 
+			// Set up the $args to be passed to the SWP_Buttons_Panel class.
 			$args = array(
 				'id' => $_POST['post_id'],
 				'buttons' => $networks,
@@ -64,14 +134,25 @@ class SWP_Buttons_Panel_Ajax {
 				'network_shares' => false
 			);
 
+			// Create the Buttons panel and render its HTML.
 			$buttons_panel = new SWP_Buttons_Panel( $args, true );
 			$buttons .= $buttons_panel->render_html();
 		}
 
-
+		// Store all of the buttons html into a local property.
 		$this->buttons_html = $buttons;
 	}
 
+
+	/**
+	 * The generate_lightbox_container() will take all of the generated html and
+	 * compile it into its complete and final form ready to send to the screen.
+	 *
+	 * @since  4.0.0 | 25 FEB 2020 | Created
+	 * @param  void
+	 * @return void Generated html is echoed directly to the screen.
+	 * 
+	 */
 	private function generate_lightbox_container() {
 		$html = '<div class="swp-lightbox-wrapper"><div class="swp-lightbox-inner">';
 		$html .= '<div class="swp-lightbox-close"></div>';
