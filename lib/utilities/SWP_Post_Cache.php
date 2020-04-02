880be1ae9e20eb04ec961eeb85d0e3766ec2ed54
Nicholas Z. Cardot <nick@sitesketch101.com>
2020-04-02 17:16:54 -0500

    Fix formatting typo in share count debugger.

--- lib/utilities/SWP_Post_Cache.php
+++ lib/utilities/SWP_Post_Cache.php
@@ -950,9 +950,9 @@ class SWP_Post_Cache {
 
 		// Output the preformatted box with the array of permalinks.
 		echo '<pre style="background:yellow;">';
-		echo '<h1>The URL\'s Being Checked:';
+		echo '<h1>The URL\'s Being Checked:</h1>';
 		var_dump($this->permalinks);
-		echo '<h1>The Responses from the API:';
+		echo '<h1>The Responses from the API:</h1>';
 		var_dump($this->raw_api_responses);
 		echo '</pre>';
 	}
