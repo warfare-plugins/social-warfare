a18ad8918c79557b10c1f9803aafc4f4e99d4b27
Nicholas Cardot <nick@sitesketch101.com>
2020-07-24 09:17:46 -0500

    Documentation and organizing for AMP.

--- lib/utilities/SWP_AMP.php
+++ lib/utilities/SWP_AMP.php
@@ -12,6 +12,7 @@
  * conditional is actually needed.
  *
  * @since 4.0.2 | 23 JUL 2020 | Created
+ *
  */
 class SWP_AMP {
 
@@ -31,10 +32,29 @@ class SWP_AMP {
 	 *
 	 */
 	public static function is_amp() {
+
+
+		/**
+		 * This is used by the AMP for WP plugin.
+		 *
+		 * Name: AMP for WP - Accelerated Mobile Pages
+		 * Link: https://wordpress.org/plugins/accelerated-mobile-pages/
+		 * Documentation: https://ampforwp.com/tutorials/article/detect-amp-page-function/
+		 *
+		 */
 		if( function_exists( 'ampforwp_is_amp_endpoint' ) && ampforwp_is_amp_endpoint() ) {
 			return true;
 		}
 
+
+		/**
+		 * This is used by the official AMP plugin.
+		 *
+		 * Name: AMP - Official AMP plugin for WordPress
+		 * Link: https://wordpress.org/plugins/amp/
+		 * Documention: https://amp-wp.org/documentation/developing-wordpress-amp-sites/how-to-develop-with-the-amp-plugin/
+		 *
+		 */
 		if( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
 		    return true;
 		}
@@ -42,17 +62,49 @@ class SWP_AMP {
 		return false;
 	}
 
+
+	/**
+	 * The hide_if_amp() method will return the supplied string if the current
+	 * page being rendered is not AMP. It will return an empty string (hence
+	 * it will hide it) if it is being accessed via an AMP endpoint.
+	 *
+	 * @since  4.0.2 | 24 JUL 2020 | Created
+	 * @param  string $text     The text to be shown or hidden.
+	 * @param  string $fallback An optional fallback to return instead of an empty string.
+	 * @return string           The original text, an empty string, or a fallback string.
+	 *
+	 */
 	public static function hide_if_amp( $text, $fallback = '' ) {
+
+		// If this is an AMP endpoint, return the fallback text.
 		if ( self::is_amp() ) {
 			return $fallback;
 		}
+
+		// If this is NOT an AMP endpoint, return the supplied text.
 		return $text;
 	}
 
+
+	/**
+	 * The display_if_amp() method will return the supplied string if the current
+	 * page being rendered is AMP. It will return an empty string if it is being
+	 * accessed via a non-AMP endpoint.
+	 *
+	 * @since  4.0.2 | 24 JUL 2020 | Created
+	 * @param  string $text     The text to be shown or hidden.
+	 * @param  string $fallback An optional fallback to return instead of an empty string.
+	 * @return string           The original text, an empty string, or a fallback string.
+	 *
+	 */
 	public static function display_if_amp( $text, $fallback = '' ) {
+
+		// If this is NOT an AMP endpoint, return the fallback text.
 		if ( false === self::is_amp() ) {
 			return $fallback;
 		}
+
+		// If this is an AMP endpoint, return the supplied text.
 		return $text;
 	}
 
