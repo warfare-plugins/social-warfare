0c5f3944c6eea3e0ee58e9313c68dbafc33496e9
Nicholas Cardot <nick@sitesketch101.com>
2020-07-29 12:43:15 -0500

    Fix for share text on embedded posts.

--- social-warfare.php
+++ social-warfare.php
@@ -19,7 +19,7 @@ defined( 'WPINC' ) || die;
  *
  */
 define( 'SWP_VERSION', '4.0.2' );
-define( 'SWP_DEV_VERSION', '2020.07.23.3:00' );
+define( 'SWP_DEV_VERSION', '2020.07.29.12:45.share-text-fix' );
 define( 'SWP_PLUGIN_FILE', __FILE__ );
 define( 'SWP_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'SWP_PLUGIN_DIR', dirname( __FILE__ ) );
