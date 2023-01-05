dac490885af27a4cbbcabd76fcc20579bfe1d2c8
Nicholas Cardot <nick@sitesketch101.com>
2023-01-05 07:33:24 -0500

    Sanitize the post type.

--- lib/utilities/SWP_Database_Migration.php
+++ lib/utilities/SWP_Database_Migration.php
@@ -254,6 +254,7 @@ class SWP_Database_Migration {
 				wp_die('You do not have authorization to view this page.');
 			}
 			$post_type = isset( $_GET['post_type'] ) ? $_GET['post_type'] : 'page';
+			$post_type = sanitize_text_field( $post_type );
 			$this->reset_post_meta_float_location( $post_type );
 		}
 
