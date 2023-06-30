2bfdfcd3f57916f142762c91bffa36ed8eaa5460
Andrii Balashov <andbalashov@hotmail.com>
2023-06-30 14:52:46 -0500

    Comments added based on WordPress standards

--- lib/social-networks/SWP_Mastodon.php
+++ lib/social-networks/SWP_Mastodon.php
@@ -1,6 +1,33 @@
 <?php
 
+/**
+ * Mastodon
+ *
+ * Class to add a Mastodon share button to the available buttons
+ *
+ * @package   SocialWarfare\Functions\Social-Networks
+ * @copyright Copyright (c) 2023, Warfare Plugins, LLC
+ * @license   GPL-3.0+
+ * @since     4.2.0 | 30 JUN 2023 | CREATED
+ *
+ */
+ 
 class SWP_Mastodon extends SWP_Social_Network {
+    /**
+	 * The Magic __construct Method
+	 *
+	 * This method is used to instantiate the social network object. It does three things.
+	 * First it sets the object properties for each network. Then it adds this object to
+	 * the globally accessible swp_social_networks array. Finally, it fetches the active
+	 * state (does the user have this button turned on?) so that it can be accessed directly
+	 * within the object.
+	 *
+	 * @since  4.2.0 | 30 JUN 2023 | CREATED
+	 * @param  none
+	 * @return none
+	 * @access public
+	 *
+	 */
 
     public function __construct() {
         $this->name    = __( 'Mastodon', 'social-warfare' );
