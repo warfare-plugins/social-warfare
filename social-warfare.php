9d77328ba5df7faa31607f641e23cf216f714e40
andbalashov <andbalashov@hotmail.com>
2024-07-25 15:08:51 -0500

    refactor: Update license information in social-warfare.php

--- social-warfare.php
+++ social-warfare.php
@@ -6,6 +6,8 @@
  * Version:     4.5.0
  * Author:      Warfare Plugins
  * Author URI:  https://warfareplugins.com
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: social-warfare
  */
 
